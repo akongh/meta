bbb4ff9c97b68e6f5f1d9a84cec5e6b8e6bd76c5
Andrei Korzhyts <pochta@afoteris.com>
2016-03-23 22:39:26 +0300

    20141210 200slov.andrej.by

--- f_perevod.php
+++ f_perevod.php
@@ -26,22 +26,33 @@ for($i = 0; $i < count($rus); $i++)
 
 		$n++;
 		}
-	if (isset($p_z))
+		
+	//if (isset($p_z) and count($p_z) == 1)
+//	{
+//		$p_z = "<span class=\"perevod\"><input type=\"checkbox\" name=\"angl[]\" checked value = '".$p[0]."'> ".$p[0]."</span><span class=\"znachenie\"> — ".$z[0]."</span>";
+//		}
+		
+	if (isset($p_z) && count($p_z) > 1)
 	{
 		$p_z = implode("<hr class=\"otbivka_0\">", $p_z);
 		$s_perevodom[$i] = "<span class = \"russk\"><input type=\"checkbox\" name=\"russk[]\" checked value = '".$rus[$i]."' hidden=\"true\">".$rus[$i]."</span><hr class=\"otbivka_6\">".$p_z;
 		}
-		else
+		else if (isset($p_z) && count($p_z) == 1)
 		{
-			$s_perevodom[$i] = "<span class = \"russk\"><input type=\"checkbox\" name=\"russk[]\" checked value = '".$rus[$i]."' hidden=\"true\">".$rus[$i]."</span><hr class=\"otbivka_6\"><span class = \"perevoda_net\">* Скоро переведём<input type=\"checkbox\" name=\"zayavka[]\" checked value = '".$rus[$i]."' hidden=\"true\"></span>";
-			if(!isset($pro_zayavku))
+		$p_z = "<span class=\"perevod\"><input type=\"checkbox\" name=\"angl[]\" checked value = '".$p[0]."'> ".$p[0]."</span><span class=\"znachenie\"> — ".$z[0]."</span>";
+		$s_perevodom[$i] = "<span class = \"russk\"><input type=\"checkbox\" name=\"russk[]\" checked value = '".$rus[$i]."' hidden=\"true\">".$rus[$i]."</span><hr class=\"otbivka_6\">".$p_z;
+			}
+			else if (!isset($p_z))
 			{
+			$s_perevodom[$i] = "<span class = \"russk\"><input type=\"checkbox\" name=\"russk[]\" checked value = '".$rus[$i]."' hidden=\"true\">".$rus[$i]."</span><hr class=\"otbivka_6\"><span class = \"perevoda_net\">* Перевода пока нет<input type=\"checkbox\" name=\"zayavka[]\" checked value = '".$rus[$i]."' hidden=\"true\"></span>";
+				if(!isset($pro_zayavku))
+				{
 				$pro_zayavku = "<span class=\"upravlenie\">* Мы&nbsp;переводим ключевые слова в&nbsp;порядке убывания по&nbsp;их&nbsp;популярности. Переводим вручную, чтобы избежать неполноценности автоматического перевода. Ключевых слов не&nbsp;одна тысяча, и&nbsp;поэтому это&nbsp;долгий и&nbsp;кропотливый процесс. И&nbsp;хоть уже&nbsp;переведено достаточно ключевых слов, чтобы охватить наиболее популярные тематики среди авторов, всё&nbsp;равно встречаются менее используемые и&nbsp;поэтому пока непереведённые ключевые слова, которые автоматически попадут в список первоочерёдных на перевод при переходе к&nbsp;получению результата строками.
 <hr class=\"otbivka_6\">
 Данное ключевое слово к&nbsp;таковым и&nbsp;относится, и,&nbsp;если оно&nbsp;ещё&nbsp;не&nbsp;в&nbsp;списке первоочерёдных на&nbsp;перевод, оно&nbsp;будет в&nbsp;него добавлено, и&nbsp;мы&nbsp;его&nbsp;переведём в&nbsp;течение двух или&nbsp;более дней, в&nbsp;зависимости от&nbsp;нашей загрузки.
 <hr class=\"otbivka_48\"></span>";
-				}
-			}
+					}
+		}
 
 	unset($p_z, $p, $z);
 	}
---
+++ shablony_soobsshenij.php
@@ -0,0 +1,9 @@
+<?php //error_reporting(0);
+session_start();
+
+include('shablony_soobsshenij.html');
+
+//echo "<pre>";
+//print_r(array_keys($_SESSION));
+//echo "</pre>";
+?>
