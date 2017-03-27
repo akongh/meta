4d06150ff5d3c1ce898151688e93afa81fe597e4
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-03-27 19:33:19 +0300

    Наброски и настройки [.htaccess] для [404.php] и [503.php]. Добавка в [robots.php]. Паятка по размещещнию Меты на хостинге в [memo.txt].

---
+++ php/503.php
@@ -0,0 +1,25 @@
+<?php
+include( $_SERVER['DOCUMENT_ROOT'] . '/meta_config.php' );
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>МЕТА. Русско-английский подбор ключевых слов для стоков</title>
+    <link href="/css/style.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="http://<?php $site_domain_name ?>/favicon.ico" type="image/ven.microsoft.ico">
+</head>
+<body>
+<div class="korobka">
+    <h1> META.afoteris.com на&nbsp;обслуживании. </h1>
+    <hr class="otbivka_96">
+    <div class="podval">
+        <hr class="otbivka_48">
+        <span class="podval_tekst">© META.afoteris.com, 2014&mdash;<?php echo date( "Y" ); ?><br>
+            <a href="mailto:pochta@afoteris.com">pochta@afoteris.com</a>
+        </span>
+        <hr class="otbivka_12">
+    </div>
+</div>
+</body>
+</html>
