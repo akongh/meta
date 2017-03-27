f864b2912c6dce594f2c327ae9d162086005410b
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-03-27 23:47:54 +0300

    Косяки и дополнения, связанные с 404 и 503.

--- meta_config_db.php
+++ meta_config_db.php
@@ -1,8 +1,10 @@
 <?php error_reporting(-1);
 
 //--[meta.afoteris.com]--
-//--[meta2.afoteris.com]--
 //include ('/home/webart/www/meta_access/db_connect.php');
+
+//--[meta2.afoteris.com]--
+//include ('/home/webart/www/meta2_access/db_connect.php');
 
 //--[meta]--
 include ($_SERVER['DOCUMENT_ROOT'].'/meta_access/db_connect.php');
