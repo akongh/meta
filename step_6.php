d01a45e34c71516ae2495fe1d8fe3890f37bc0f0
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-03-28 14:37:44 +0300

    - undefined (результат на англ.) на 6-м шаге.

--- step_6.php
+++ step_6.php
@@ -1,21 +1,20 @@
-<?php error_reporting(-1);
+<?php error_reporting( - 1 );
 session_start();
 include( $_SERVER['DOCUMENT_ROOT'] . '/meta_config.php' );
 
-if(!isset($_SESSION["metka"]))
-{
-	header("Location: http://".$site_domain_name."/index.php");
-	}
+if ( ! isset( $_SESSION["metka"] ) ) {
+    header( "Location: http://" . $site_domain_name . "/index.php" );
+}
 
 $kol_slov_russk = $_SESSION["kol_slov_russk"];
-$kol_slov_angl = $_SESSION["kol_slov_angl"];
+$kol_slov_angl  = $_SESSION["kol_slov_angl"];
 
 $_REZULTAT_russk = $_SESSION["_REZULTAT_russk"];
-$_REZULTAT_angl = $_SESSION["_REZULTAT_angl"];
-
-if(isset($_SESSION["_REZULTAT_russk_neperevedennye"]))
-{
-	$_REZULTAT_russk_neperevedennye = $_SESSION["_REZULTAT_russk_neperevedennye"];
-	}
+if ( isset( $_SESSION["_REZULTAT_angl"] ) ) {
+    $_REZULTAT_angl = $_SESSION["_REZULTAT_angl"];
+};
+if ( isset( $_SESSION["_REZULTAT_russk_neperevedennye"] ) ) {
+    $_REZULTAT_russk_neperevedennye = $_SESSION["_REZULTAT_russk_neperevedennye"];
+}
 
 include( 'html/step_6.html' );
