1abf7fd0ca7ce9305e9e38835dbe198c4df002ad
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-03-28 16:06:27 +0300

    На главной странице сделан точный подсчёт переведённых ключевых слов.

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php error_reporting(-1);
+<?php error_reporting( - 1 );
 session_start();
 session_unset();
-unset($_POST);
+unset( $_POST );
+include( $_SERVER['DOCUMENT_ROOT'] . '/meta_config_db.php' );
 include( $_SERVER['DOCUMENT_ROOT'] . '/meta_config.php' );
+
+$SQL_count_translated_words_query = mysqli_query( $db_connect, "
+	select count(`k-ts`.`s`)
+	from `k-ts`
+	where `f` = 1
+	" );
+
+$data                       = mysqli_fetch_array( $SQL_count_translated_words_query );
+$SQL_count_translated_words = number_format( $data[0], 0, '', ' ' );
+
 include( 'html/meta.html' );
