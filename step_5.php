a05045d11c38d2bdc99f410c4dd27f331e69ac0a
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-03-30 19:28:50 +0300

    Очередная серия, найденых Леной [Undefined-ов].

--- step_5.php
+++ step_5.php
@@ -5,7 +5,9 @@ include( $_SERVER['DOCUMENT_ROOT'] . '/meta_config.php' );
 if ( ! isset( $_SESSION["metka"] ) ) {
     header( "Location: http://" . $site_domain_name . "/index.php" );
 }
-$s_perevodom = $_SESSION["s_perevodom"];
+if ( isset( $_SESSION["s_perevodom"] ) ) {
+    $s_perevodom = $_SESSION["s_perevodom"];
+};
 if ( isset( $_SESSION["pro_zayavku"] ) ) {
     $pro_zayavku = $_SESSION["pro_zayavku"];
 };
--- step_6.php
+++ step_6.php
@@ -6,10 +6,15 @@ if ( ! isset( $_SESSION["metka"] ) ) {
     header( "Location: http://" . $site_domain_name . "/index.php" );
 }
 
-$kol_slov_russk = $_SESSION["kol_slov_russk"];
-$kol_slov_angl  = $_SESSION["kol_slov_angl"];
-
-$_REZULTAT_russk = $_SESSION["_REZULTAT_russk"];
+if ( isset( $_SESSION["kol_slov_russk"] ) ) {
+    $kol_slov_russk = $_SESSION["kol_slov_russk"];
+}
+if ( isset( $_SESSION["kol_slov_angl"] ) ) {
+    $kol_slov_angl = $_SESSION["kol_slov_angl"];
+}
+if ( isset( $_SESSION["_REZULTAT_russk"] ) ) {
+    $_REZULTAT_russk = $_SESSION["_REZULTAT_russk"];
+}
 if ( isset( $_SESSION["_REZULTAT_angl"] ) ) {
     $_REZULTAT_angl = $_SESSION["_REZULTAT_angl"];
 };
