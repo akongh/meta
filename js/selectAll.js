2b2739a2b253bfa61fd458c0d6967281e0b8ebfb
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-07 19:26:34 +0300

    На втором шаге сделан выбор всех флагов при щелчке на заголовке.

---
+++ js/selectAll.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Andrei on 07.04.2017.
+ */
+
+var title = document.getElementsByTagName("h1");
+title[0].addEventListener("click", selectAll);
+
+
+function selectAll() {
+    var keywordCheckbox = document.getElementsByName("slova_s_flagom[]");
+    for (var i = 0; i <= keywordCheckbox.length; i++) {
+        keywordCheckbox[i].checked = true;
+    }
+    ;
+};
