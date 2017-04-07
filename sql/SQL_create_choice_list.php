438d229ba252cf5cb2579ec3157746eec0e2f9b1
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-07 18:07:01 +0300

    Исправлена логическая ошибка в запросе подбора [sql/SQL_create_choice_list.php].

--- sql/SQL_create_choice_list.php
+++ sql/SQL_create_choice_list.php
@@ -1,17 +1,17 @@
 <?php error_reporting( - 1 );
 
-$_SQL_zapros_podbor = "select `k-ts`.`s`, count(*)    
-	  from (    
-		select `k-t_s`.`id_n`    
-		from  `k-ts`    
-		join `k-t_s` on `k-t_s`.`id_s` = `k-ts`.`ids` 
+$_SQL_zapros_podbor = "select `k-ts`.`s`, count(*)
+	  from (
+		select `k-t_s`.`id_n`
+		from  `k-ts`
+		join `k-t_s` on `k-t_s`.`id_s` = `k-ts`.`ids`
 		where `k-ts`.`s` in ('" . $_SQL_stroka_dlya_podbora . "')
-		group by `k-t_s`.`id_n` having count(/*distinct*/ `k-t_s`.`id_s`) = '" . $i . "'    
-		) `g`   
-	  join `k-t_s` on `k-t_s`.`id_n` = `g`.`id_n`    
+		group by `k-t_s`.`id_n` having count(/*distinct*/ `k-t_s`.`id_s`) >= '" . $i . "'
+		) `g`
+	  join `k-t_s` on `k-t_s`.`id_n` = `g`.`id_n`
 	  join `k-ts` on `k-ts`.`ids` = `k-t_s`.`id_s`
 	  where `k-ts`.`f` in (0, 1, 7)
-	  group by `k-t_s`.`id_s`, `k-ts`.`s`    
+	  group by `k-t_s`.`id_s`, `k-ts`.`s`
 	  order by count(*) desc, `k-ts`.`s` LIMIT 0," . $granicza . "
 	  ;
 	  ";
