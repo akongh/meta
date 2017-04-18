592528eaeed0f764740d429a67534d6aaeea15ad
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-18 23:39:28 +0300

    [SQs]. Начало.

---
+++ meta_admin/sqs/gsqSS.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei
+ * Date: 18.04.2017
+ * Time: 22:11
+ */
+
+$url = "https://www.shutterstock.com/api/autocomplete?q=flovers&mediaType=image&_=1492495477024";
+$ses = curl_init();
+curl_setopt($ses, CURLOPT_URL, $url);
+curl_setopt($ses, CURLOPT_RETURNTRANSFER,true);
+echo $data = curl_exec($ses);
+curl_close($ses);
