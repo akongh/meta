42886c4e3b597603a1d2ecea8b420799c6859305
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-19 08:11:04 +0300

    [SQs] _ создана страница для ввода ключевого слова и получения подсказок от Шаттерстока.

--- meta_admin/sqs/sqs_shutterstock.php
+++ meta_admin/sqs/sqs_shutterstock.php
@@ -7,22 +7,18 @@
  */
 
 //задаём переменные
-$kw         = "lena";
+if ( isset( $_POST["kw"] ) ) {
+    $kw = $_POST["kw"];
+};
+//Заменяем пробелы на «+» для строки запроса
+$kw         = preg_replace( "/ /", "+", $kw );
 $media_type = "image";
 $fake_time  = time() - rand( 14400, 43200 );
 $fake_num   = rand( 100, 999 );
 $id         = $fake_time . $fake_num;//TODO: Пока тупая имитация, выяснить истинное формирование этого значения. Похоже, оно нужно для статистики или для отслеживания автозапросов. Пока не перезагрузишь страницу, часть с временем не меняется, а добавка растёт на единицу при каждом запросе.
 //TODO: Возможно, имеет смысл попытаться подменить другие данные (о клиенте и т. д.), которые передаются или наоборот, добавить, чтоб не было видно, что запросы с сервера.
-$url = "https://www.shutterstock.com/api/autocomplete?q=" . $kw . "&mediaType=" . $media_type . "&_=" . $id;
-
-print_r( "<pre>" );
-
-echo( $kw );
-echo( "<br>" );
-echo( "<br>" );
-echo( $fake_time . " - [ " . date( 'd.m.Y, H:i:s', $fake_time ) . " ] - " . $fake_num );
-echo( "<br>" );
-echo( "<br>" );
+$url           = "https://www.shutterstock.com/api/autocomplete?q=" . $kw . "&mediaType=" . $media_type . "&_=" . $id;
+$fake_time_num = $fake_time . " - [ " . date( 'd.m.Y, H:i:s', $fake_time ) . " ] - " . $fake_num;
 
 //запрос-ответ
 $ses = curl_init();
@@ -35,9 +31,14 @@ curl_close( $ses );
 $format_data = json_decode( $data, true );
 $format_data = $format_data["data"];
 $format_data = $format_data["autocompletions"];
-//вывод строк «ключ-значение»
+//создание массива строк «ключ-значение»
 for ( $i = 0; $i < count( $format_data ); $i ++ ) {
-    echo( $format_data[ $i ]["pattern"] . " - " . $format_data[ $i ]["probability"] . "<br>" );
+    $format_data_arr[ $i ] = "<span class='bold'>" . $format_data[ $i ]["pattern"] . "</span>" . " - " . $format_data[ $i ]["probability"];
 }
+//создание строки «ключ-значение»
+$format_data_string = implode( "<br>", $format_data_arr );
 
-print_r( "</pre>" );
+//возвращаем пробелы после использования в запросе
+$kw         = preg_replace( "/\+/", " ", $kw );
+
+include( 'sqs.html' );
