f9e42d0c332e36164f82dc81f29e2b09d36b6817
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-19 16:11:38 +0300

    [SQs] _ Выбор типа произведений для поиска.

--- meta_admin/sqs/sqs_shutterstock.php
+++ meta_admin/sqs/sqs_shutterstock.php
@@ -6,44 +6,47 @@
  * Time: 22:11
  */
 
-//задаём переменные
+//Задаём переменные
 if ( isset( $_POST["kw"] ) && ! preg_match( "/^([а-яё\s\-]+)$/iu", $_POST["kw"] ) ) {//проверка на отсутствие кирилицы
     $kw = $_POST["kw"];
 } else {
     $kw = "";
 };
-//Заменяем пробелы на «+» для строки запроса
-$kw         = preg_replace( "/ /", "+", $kw );
-$media_type = "image";
-$fake_time  = time() - rand( 14400, 43200 );
-$fake_num   = rand( 100, 999 );
-$id         = $fake_time . $fake_num;//TODO: Пока тупая имитация, выяснить истинное формирование этого значения. Похоже, оно нужно для статистики или для отслеживания автозапросов. Пока не перезагрузишь страницу, часть с временем не меняется, а добавка растёт на единицу при каждом запросе.
+$kw = preg_replace( "/ /", "+", $kw );//заменяем пробелы на «+» для строки запроса
+if ( isset( $_POST["mt"] ) ) {
+    $mt = $_POST["mt"];
+} else {
+    $mt = "image";
+};
+$fake_time = time() - rand( 14400, 43200 );
+$fake_num  = rand( 100, 999 );
+$id        = $fake_time . $fake_num;//TODO: Пока тупая имитация, выяснить истинное формирование этого значения. Похоже, оно нужно для статистики или для отслеживания автозапросов. Пока не перезагрузишь страницу, часть с временем не меняется, а добавка растёт на единицу при каждом запросе.
 //TODO: Возможно, имеет смысл попытаться подменить другие данные (о клиенте и т. д.), которые передаются или наоборот, добавить, чтоб не было видно, что запросы с сервера.
-$url           = "https://www.shutterstock.com/api/autocomplete?q=" . $kw . "&mediaType=" . $media_type . "&_=" . $id;
+$url           = "https://www.shutterstock.com/api/autocomplete?q=" . $kw . "&mediaType=" . $mt . "&_=" . $id;
 $fake_time_num = $fake_time . " - [ " . date( 'd.m.Y, H:i:s', $fake_time ) . " ] - " . $fake_num;
 
-//запрос-ответ
+//Запрос-ответ
 $ses = curl_init();
 curl_setopt( $ses, CURLOPT_URL, $url );
 curl_setopt( $ses, CURLOPT_RETURNTRANSFER, true );
 $data = curl_exec( $ses );
 curl_close( $ses );
 
-//форматирование json-ответа
+//Форматирование json-ответа
 $format_data = json_decode( $data, true );
 $format_data = $format_data["data"];
 $format_data = $format_data["autocompletions"];
 
-//создание массива строк «ключ-значение»
+//Создание массива строк «ключ-значение»
 for ( $i = 0; $i < count( $format_data ); $i ++ ) {
     $format_data_arr[ $i ] = "<tr><td class='table-sqs-patterns'><span class='bold'>" . $format_data[ $i ]["pattern"] . "</span></td><td>" . $format_data[ $i ]["probability"] . "</td></tr>";
 }
-//создание строки «ключ-значение»
+//Создание строки «ключ-значение»
 if ( isset( $format_data_arr ) ) {
     $format_data_string = "<table class='table-sqs'>" . implode( "", $format_data_arr ) . "</table>";
 }
 
-//возвращаем пробелы после использования в запросе
+//Возвращаем пробелы после использования в запросе
 //$kw = preg_replace( "/\+/", " ", $kw );
 
 include( 'sqs.html' );
