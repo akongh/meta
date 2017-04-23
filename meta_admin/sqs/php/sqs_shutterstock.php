e784f8e09ae6b103eef53d415bcffc6061f23167
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-23 12:01:04 +0300

    [SQs] _ Подключение к Шаттерстоку и получение подсказок вынесено в отдельную функцию [get_hints( _PARAM_kw_query, _PARAM_mt )].

--- meta_admin/sqs/php/sqs_shutterstock.php
+++ meta_admin/sqs/php/sqs_shutterstock.php
@@ -1,10 +1,12 @@
 <?php error_reporting( - 1 );
 
+
 if ( isset( $_POST["mtRadio"] ) ) {
     $mt = $_POST["mtRadio"];
 } else {
     $mt = "image";
 };
+
 
 if ( isset( $_POST["kwString"] ) ) {
     $kwArr = mb_strtolower( htmlspecialchars( strip_tags( stripslashes( $_POST["kwString"] ) ) ), "utf-8" );
@@ -16,45 +18,28 @@ if ( isset( $_POST["kwString"] ) ) {
     $kwArr = array_values( array_unique( ( array_diff( $kwArr, array( '' ) ) ) ) );
     if ( count( $kwArr ) > 0 ) {
         for ( $i = 0; $i < count( $kwArr ); $i ++ ) {
-            $kw_query       = preg_replace( "/ /", "+", $kwArr[ $i ] );
-            $anticache_time = time();
-            $anticache_num  = rand( 100, 999 );
-            $anticache_id   = $anticache_time . $anticache_num;
-            //TODO: Возможно, имеет смысл подменить другие данные (о клиенте и т. д.), которые передаются или наоборот, добавить, чтоб не было видно, что запросы с сервера.
-            $url = "https://www.shutterstock.com/api/autocomplete?q=" . $kw_query . "&mediaType=" . $mt . "&_=" . $anticache_id;
-            $ses = curl_init();
-            curl_setopt( $ses, CURLOPT_URL, $url );
-            curl_setopt( $ses, CURLOPT_RETURNTRANSFER, true );
-            $data = curl_exec( $ses );
-            curl_close( $ses );
-
-            echo( $data );
+            get_hints( $kwArr[ $i ], $mt );
             if ( $i > 0 ) {
                 sleep( 2 );
             }
         }
     } else {
-        $kw_query       = "";
-        $anticache_time = time();
-        $anticache_num  = rand( 100, 999 );
-        $anticache_id   = $anticache_time . $anticache_num;
-        //TODO: Возможно, имеет смысл подменить другие данные (о клиенте и т. д.), которые передаются или наоборот, добавить, чтоб не было видно, что запросы с сервера.
-        $url = "https://www.shutterstock.com/api/autocomplete?q=" . $kw_query . "&mediaType=" . $mt . "&_=" . $anticache_id;
-        $ses = curl_init();
-        curl_setopt( $ses, CURLOPT_URL, $url );
-        curl_setopt( $ses, CURLOPT_RETURNTRANSFER, true );
-        $data = curl_exec( $ses );
-        curl_close( $ses );
-
-        echo( $data );
+        get_hints( "", $mt );
     }
 } else {
-    $kw_query       = "";
+    get_hints( "", $mt );
+};
+
+
+function get_hints( $_PARAM_kw_query, $_PARAM_mt ) {
+    if ( $_PARAM_kw_query != "" ) {
+        $_PARAM_kw_query = preg_replace( "/ /", "+", $_PARAM_kw_query );
+    };
     $anticache_time = time();
     $anticache_num  = rand( 100, 999 );
     $anticache_id   = $anticache_time . $anticache_num;
     //TODO: Возможно, имеет смысл подменить другие данные (о клиенте и т. д.), которые передаются или наоборот, добавить, чтоб не было видно, что запросы с сервера.
-    $url = "https://www.shutterstock.com/api/autocomplete?q=" . $kw_query . "&mediaType=" . $mt . "&_=" . $anticache_id;
+    $url = "https://www.shutterstock.com/api/autocomplete?q=" . $_PARAM_kw_query . "&mediaType=" . $_PARAM_mt . "&_=" . $anticache_id;
     $ses = curl_init();
     curl_setopt( $ses, CURLOPT_URL, $url );
     curl_setopt( $ses, CURLOPT_RETURNTRANSFER, true );
@@ -62,4 +47,6 @@ if ( isset( $_POST["kwString"] ) ) {
     curl_close( $ses );
 
     echo( $data );
-};
+}
+
+;
