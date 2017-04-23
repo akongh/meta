d8c64fab8c9feedeb9097aab41a803fd96fb0481
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-23 03:51:51 +0300

    [SQs] _ Утраны лишние [trim], удалён устаревший код.

--- meta_admin/sqs/php/sqs_shutterstock.php
+++ meta_admin/sqs/php/sqs_shutterstock.php
@@ -1,5 +1,6 @@
 <?php error_reporting( - 1 );
 
+
 //Задаём переменные
 if ( isset( $_POST["kwString"] ) && ! preg_match( "/^([а-яё]+)$/iu", $_POST["kwString"] ) ) {//проверка на отсутствие кирилицы
     $kw = preg_replace( "/ {2,}/", " ", trim( $_POST["kwString"] ) );//убираем крайние и двойные пробелы
@@ -14,10 +15,11 @@ if ( isset( $_POST["mtRadio"] ) ) {
 };
 $fake_time = time() - rand( 14400, 43200 );
 $fake_num  = rand( 100, 999 );
-$id        = $fake_time . $fake_num;//TODO: Пока тупая имитация, выяснить истинное формирование этого значения. Похоже, оно нужно для статистики или для отслеживания автозапросов. Пока не перезагрузишь страницу, часть с временем не меняется, а добавка растёт на единицу при каждом запросе.
+$id        = $fake_time . $fake_num;
+//TODO: Пока тупая имитация, выяснить истинное формирование этого значения. Похоже, оно нужно для статистики или для отслеживания автозапросов. Пока не перезагрузишь страницу, часть с временем не меняется, а добавка растёт на единицу при каждом запросе.
 //TODO: Возможно, имеет смысл попытаться подменить другие данные (о клиенте и т. д.), которые передаются или наоборот, добавить, чтоб не было видно, что запросы с сервера.
-$url           = "https://www.shutterstock.com/api/autocomplete?q=" . $kw_query . "&mediaType=" . $mt . "&_=" . $id;
-$fake_time_num = $fake_time . " - [ " . date( 'd.m.Y, H:i:s', $fake_time ) . " ] - " . $fake_num;
+$url = "https://www.shutterstock.com/api/autocomplete?q=" . $kw_query . "&mediaType=" . $mt . "&_=" . $id;
+
 
 //Запрос-ответ
 $ses = curl_init();
@@ -25,6 +27,7 @@ curl_setopt( $ses, CURLOPT_URL, $url );
 curl_setopt( $ses, CURLOPT_RETURNTRANSFER, true );
 $data = curl_exec( $ses );
 curl_close( $ses );
+
 
 //Форматирование json-ответа
 $format_data = json_decode( $data, true );
@@ -40,12 +43,13 @@ $format_data = $format_data["data"]["autocompletions"];
 //}
 
 for ( $i = 0; $i < count( $format_data ); $i ++ ) {
-    $format_data_arr[ $i ] = trim( str_replace( trim( $kw ) . " ", "", $format_data[ $i ]["pattern"] ) ) . " - " . $format_data[ $i ]["probability"];
+    $format_data_arr[ $i ] = str_replace( $kw . " ", "", $format_data[ $i ]["pattern"] ) . " - " . $format_data[ $i ]["probability"];//TODO: А если вырежет в середине ответа?
 }
 if ( isset( $format_data_arr ) ) {
     $format_data_str = implode( ", ", $format_data_arr );
 } else {
     $format_data_str = "Нет подсказок.";
 }
+
 
 echo( $format_data_str );
