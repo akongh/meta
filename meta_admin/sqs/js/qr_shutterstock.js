549a6deba7441f2f70df13b3db5642572d9c06da
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-04-24 07:55:49 +0300

    [SQs] _ +[function json_parser()], которая разбирает ответ Шаттерстока и по правилам удаляет из него опорные ключевые слова.

--- meta_admin/sqs/js/qr_shutterstock.js
+++ meta_admin/sqs/js/qr_shutterstock.js
@@ -26,29 +26,9 @@ function sendQuery(url) {
 };
 
 
-
-
-
-//Форматирование json-ответа
-//$format_data = json_decode( $data, true );
-//$format_data = $format_data["data"]["autocompletions"];
-
-
-//for ( $i = 0; $i < count( $format_data ); $i ++ ) {
-//    $format_data_arr[ $i ] = str_replace( $kw . " ", "", $format_data[ $i ]["pattern"] ) . " - " . $format_data[ $i ]["probability"];//TODO: А если вырежет в середине ответа?
-//}
-//if ( isset( $format_data_arr ) ) {
-//    $format_data_str = implode( ", ", $format_data_arr );
-//} else {
-//    $format_data_str = "Нет подсказок.";
-//}
-
-
-//Создание массива строк «ключ-значение»
-//for ( $i = 0; $i < count( $format_data ); $i ++ ) {
-//    $format_data_arr[ $i ] = "<tr><td class='table-sqs-patterns'><span id='pattern-kw' class='bold kw-pattern'>" . trim( str_replace( trim($kw) . " ", "", $format_data[ $i ]["pattern"] ) ) . "</span></td><td>" . $format_data[ $i ]["probability"] . "</td></tr>\n";
-//}
-////Создание строки «ключ-значение»
-//if ( isset( $format_data_arr ) ) {
-//    $format_data_string = "<table class='table-sqs'>\n" . implode( "", $format_data_arr ) . "</table>";
-//}
+// for ( $i = 0; $i < count( $format_data ); $i ++ ) {
+//     $format_data_arr[ $i ] = "<tr><td class='table-sqs-patterns'><span id='pattern-kw' class='bold kw-pattern'>" . trim( str_replace( trim( $kw ) . " ", "", $format_data[ $i ]["pattern"] ) ) . "</span></td><td>" . $format_data[ $i ]["probability"] . "</td></tr>\n";
+// }
+// if ( isset( $format_data_arr ) ) {
+//     $format_data_string = "<table class='table-sqs'>\n" . implode( "", $format_data_arr ) . "</table>";
+// }
--- meta_admin/sqs/php/sqs_shutterstock.php
+++ meta_admin/sqs/php/sqs_shutterstock.php
@@ -12,16 +12,19 @@ if ( isset( $_POST["kwString"] ) ) {
     $kwArr = prepare_query( $_POST["kwString"] );
     if ( count( $kwArr ) > 0 ) {
         for ( $i = 0; $i < count( $kwArr ); $i ++ ) {
-            get_hints( $kwArr[ $i ], $mt );
+            $json_data = get_hints( $kwArr[ $i ], $mt );
+            json_parser( $json_data, $kwArr[ $i ] );
             if ( $i > 0 ) {
                 sleep( 2 );
             }
         }
     } else {
-        get_hints( "", $mt );
+        $json_data = get_hints( "", $mt );
+        json_parser( $json_data, "" );
     }
 } else {
-    get_hints( "", $mt );
+    $json_data = get_hints( "", $mt );
+    json_parser( $json_data, "" );
 };
 
 
@@ -55,10 +58,47 @@ function get_hints( $_PARAM_kw_query, $_PARAM_mt ) {
     $ses = curl_init();
     curl_setopt( $ses, CURLOPT_URL, $url );
     curl_setopt( $ses, CURLOPT_RETURNTRANSFER, true );
-    $data = curl_exec( $ses );
+    $json_data = curl_exec( $ses );
     curl_close( $ses );
 
-    echo( $data );
+    return $json_data;
+}
+
+;
+
+
+function json_parser( $_PARAM_json_data, $_PARAM_kw ) {
+    $formated_data = json_decode( $_PARAM_json_data, true );
+    $formated_data = $formated_data["data"]["autocompletions"];
+    if ( isset( $formated_data ) && count( $formated_data ) > 0 ) {
+        $rulesArr = [
+            " and ",
+            " at ",
+            " in ",
+            " on ",
+            " of "
+        ];
+        for ( $i = 0; $i < count( $formated_data ); $i ++ ) {
+            $f = true;
+            for ( $j = 0; $j < count( $rulesArr ); $j ++ ) {
+                if ( strpos( $formated_data[ $i ]["pattern"], $rulesArr[ $j ] ) === false ) {
+                    continue;
+                } else {
+                    $f = false;
+                    break;
+                }
+            }
+            if ( $f === true ) {
+                //TODO: А если вырежет в середине ответа?
+                $formated_data[ $i ]["pattern"] = str_replace( $_PARAM_kw . " ", "", $formated_data[ $i ]["pattern"] );
+            }
+            $formated_data[ $i ] = $formated_data[ $i ]["pattern"] . " - " . $formated_data[ $i ]["probability"];
+        }
+        $formated_data = implode( "\n", $formated_data );
+        echo( "<pre>" . $formated_data . "</pre>" );
+    } else {
+        echo( "Подсказок нет." );
+    };
 }
 
 ;
