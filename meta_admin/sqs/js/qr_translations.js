f46185eeb56360fc243c83c236ad8f62dd6204d3
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-05-01 14:34:57 +0300

    [SQs] _ Добавлен перевод русского ОКС на английский для использования в запросе подбора.

--- meta_admin/sqs/js/qr_translations.js
+++ meta_admin/sqs/js/qr_translations.js
@@ -22,13 +22,21 @@ function sendQueryGetTranslationsCreateHTMLTranslationsList(PARAM_url) {
         if (request.readyState === 4 && request.status === 200) {
             if (request.responseText === "-1") {
                 document.querySelector("#translations-area").innerHTML = "Перевода нет.";
+            } else if (request.responseText === "-2") {
+                document.querySelector("#translations-area").innerHTML = "Нечего переводить.";
+            } else if (request.responseText === "-3") {
+                document.querySelector("#translations-area").innerHTML = "Используйте только кирилицу и цифры.";
             } else {
+                document.querySelector("#translations-area").innerHTML = JSON.parse(request.responseText).join("");
 
-                var resultArray = JSON.parse(request.responseText);
-
-                document.querySelector("#translations-area").innerHTML = createHTMLTranslationsList(resultArray);
-
-
+                var hintKeywords = document.querySelectorAll("#hint-keyword");
+                for (var i = 0; i < hintKeywords.length; i++) {
+                    hintKeywords[i].addEventListener("click", function (e) {
+                        e.stopPropagation();
+                    }, false);
+                    hintKeywords[i].addEventListener("click", keywordwPatternToQuery);
+                }
+                ;
             }
             ;
         }
@@ -37,9 +45,5 @@ function sendQueryGetTranslationsCreateHTMLTranslationsList(PARAM_url) {
     request.open("POST", PARAM_url, true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send(keywordInRussian);
-};
-
-
-function createHTMLTranslationsList(PARAM_translationsAray) {
-    return PARAM_translationsAray;
-};
+}
+;
--- meta_admin/sqs/php/ex_translations.php
+++ meta_admin/sqs/php/ex_translations.php
@@ -1,6 +1,89 @@
 <?php error_reporting( - 1 );
 
-//получаем и определяем ОКС на русском ящыке для перевода
-$keyword_russian = $_POST["keywordInRussian"];
+//получаем и определяем ОКС на русском языке для перевода
+$keyword_in_russian = $_POST["keywordInRussian"];
 
-echo( "-1" );
+
+//готовим ОКС на русском для зопроса переводов
+$keyword_in_russian = PREPARE_KEYWORD_IN_RUSSIAN_FOR_TRANLATION( $keyword_in_russian );
+
+
+//нечего переводить
+if ( $keyword_in_russian == "" ) {
+    echo( "-2" );
+    exit;
+};
+
+
+//только кирилица и цифры
+if ( ! preg_match( "/^([а-яё0-9\s\-]+)$/iu", $keyword_in_russian ) ) {
+    echo( "-3" );
+    exit;
+};
+
+
+//попытка найти для полученного ОКС переводы
+include( $_SERVER['DOCUMENT_ROOT'] . '/meta_config_db.php' );
+$translations_array = SEARCH_TRANLATIONS( $db_connect, $keyword_in_russian );
+mysqli_close( $db_connect );
+
+
+//перевод не найден
+if ( $translations_array == "-1" ) {
+    echo( "-1" );
+    exit;
+};
+
+
+//подготовка json-ответа
+$json_result = json_encode( $translations_array, JSON_UNESCAPED_UNICODE );
+
+
+echo $json_result;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ФУНКЦИИ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//проверяет, чистит и правит полученное ОКС
+function PREPARE_KEYWORD_IN_RUSSIAN_FOR_TRANLATION( $PARAM_keyword_in_russian ) {
+    $keyword_in_russian = mb_strtolower( htmlspecialchars( strip_tags( stripslashes( $PARAM_keyword_in_russian ) ) ), "utf-8" );
+    $keyword_in_russian = trim( preg_replace( "/ {2,}/", " ", $keyword_in_russian ) );
+
+    return $keyword_in_russian;
+}
+
+;
+
+
+//ищет в базе переводы
+function SEARCH_TRANLATIONS( $PARAM_db_connect, $PARAM_keyword_in_russian ) {
+
+    $SQL_select_translations_and_sense = mysqli_query( $PARAM_db_connect, "
+	select `l-ts`.`s`, `tz`.`z`
+	from `k-ts`
+	join `k_l` on `k-ts`.`ids`=`k_l`.`idk`
+	join `l-ts` on `l-ts`.`ids`=`k_l`.`idl`
+	join `tz` on `tz`.`idz`=`k_l`.`idz`
+	where `k-ts`.`s`='" . $PARAM_keyword_in_russian . "'
+	" );
+
+    $n = 0;
+    while ( $result = mysqli_fetch_array( $SQL_select_translations_and_sense ) ) {
+        $translation[ $n ]        = $result['s'];
+        $sense[ $n ]              = $result['z'];
+        $translations_array[ $n ] = "
+        <span id='hint-keyword' class='hint-keyword'>" . $translation[ $n ] . "</span> - " . $sense[ $n ] . "<br>";
+        $n ++;
+    };
+
+    if ( isset( $translations_array ) || count( $translations_array ) > 0 ) {
+        return $translations_array;
+    } else {
+        return "-1";
+    };
+}
+
+;
