d2cc8a686123b5503ad21181e7a14c7ba1a81aa6
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-05-07 14:22:54 +0300

    Статистика по подсказкам на главной странице Управления.

--- meta_admin/index.php
+++ meta_admin/index.php
@@ -8,11 +8,15 @@ $kir_kol_naborov_otvet       = mysqli_fetch_row( $kir_kol_naborov_zapros );
 $lat_kol_slov_otvet          = mysqli_fetch_row( $lat_kol_slov_zapros );
 $na_zayavke_otvet            = mysqli_fetch_row( $na_zayavke_zapros );
 $perevedeno_otvet            = mysqli_fetch_row( $perevedeno_zapros );
+$hint_translation_response   = mysqli_fetch_row( $hint_translation_querry );
+$hint_request_response       = mysqli_fetch_row( $hint_request_querry );
 $_SESSION["kir_kol_slov"]    = $kir_kol_slov_otvet[0];
 $_SESSION["kir_kol_naborov"] = $kir_kol_naborov_otvet[0];
 $_SESSION["lat_kol_slov"]    = $lat_kol_slov_otvet[0];
 $na_zayavke                  = $na_zayavke_otvet[0];
 $perevedeno                  = $perevedeno_otvet[0];
+$hint_translation            = $hint_translation_response[0];
+$hint_request                = $hint_request_response[0];
 
 $kir_kol_slov    = $_SESSION["kir_kol_slov"];
 $kir_kol_naborov = $_SESSION["kir_kol_naborov"];
