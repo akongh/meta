75de747c451f543ccddb864bc1cf72a3ed9934a6
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-05-23 07:22:42 +0300

    ПКС: добавил юзерагента.

--- selling-keywords/selling_keywords.php
+++ selling-keywords/selling_keywords.php
@@ -15,7 +15,8 @@ $array_works_data = ARRAY_WORKS_DATA( $keyword, $image_type );
 //echo( $array_works_data );
 
 $array_useragents = [
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.97 Safari/537.36 Vivaldi/1.9.818.49'
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.97 Safari/537.36 Vivaldi/1.9.818.49',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36 OPR/45.0.2552.812'
 ];
 
 $array_cookies = [
