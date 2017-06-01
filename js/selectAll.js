930e1f49e3fbec1217663c088faa99e9186e259f
Andrei Korzhyts <vesti.akp@yandex.ru>
2017-06-01 10:33:42 +0300

    Исправлена логическая ошибка при сравнении счётчика с длиной массива.

--- js/selectAll.js
+++ js/selectAll.js
@@ -4,7 +4,7 @@ title[0].addEventListener("click", selectAll);
 
 function selectAll() {
     var keywordCheckbox = document.getElementsByName("slova_s_flagom[]");
-    for (var i = 0; i <= keywordCheckbox.length; i++) {
+    for (var i = 0; i < keywordCheckbox.length; i++) {
         keywordCheckbox[i].checked = true;
     }
     ;
