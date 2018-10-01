50c4fa97728c584a03cc8ba4f75eca2275b123bd
Andrei Korzhyts <andreikorzhyts@afoteris.com>
2018-10-01 15:59:36 +0300

    Правка параметров соединения с БД.

--- access_meta/db_connect.php
+++ access_meta/db_connect.php
@@ -1,16 +1,16 @@
 <?php error_reporting( - 1 );
 
 //--[meta.afoteris.com]--
-//$db_server = "by122.atservers.net";
-//$db_user = "user2031505";
-//$db_password = "813p9VQSHSc6";
-//$db_name = "user2031505_meta";
+//$db_server   = "mysql5.activeby.net";
+//$db_user     = "user2031505";
+//$db_password = "BAGjPYahfJUHDMHV";
+//$db_name     = "user2031505_meta";
 
 //--[meta]--
-$db_server = "127.0.0.1";
-$db_user = "root";
+$db_server   = "127.0.0.1";
+$db_user     = "root";
 $db_password = "root";
-$db_name = "_meta";
+$db_name     = "_meta";
 
 $db_connect = mysqli_connect( $db_server, $db_user, $db_password, $db_name );
 mysqli_query( $db_connect, "SET character_set_database=utf8" );
