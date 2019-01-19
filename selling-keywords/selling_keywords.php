db35c81dae680c8e15678d47b3f67afe4393381c
Andrei Korzhyts <andreikorzhyts@afoteris.com>
2019-01-19 23:43:30 +0300

    Обновление сессии.

--- selling-keywords/selling_keywords.php
+++ selling-keywords/selling_keywords.php
@@ -29,7 +29,7 @@ $array_useragents = [
 
 $array_cookies = [
     'session=s%3ANkSAzpDTwBw3MGVyKodbcqU4ffcvIvzZ.FQYPk92fQvM3MVcH9XFuYtYKvWXue0FPQQf3Si%2FC58g',
-    'session=s%3ANkSAzpDTwBw3MGVyKodbcqU4ffcvIvzZ.FQYPk92fQvM3MVcH9XFuYtYKvWXue0FPQQf3Si%2FC58g'
+    'session=s%3A8FXrLwD1i8gqH2LbObNPFVF85-1bF_Fi.kdJfhAHaHSRLnTf8YEXyuI2GksFCJmmwy%2FAOayhOQ%2B8'
 ];
 
 
