45a547970b6b7953c7638fbaf5df8913cc873e5d
Andrei Korzhyts <andreikorzhyts@afoteris.com>
2019-02-10 15:49:31 +0300

    comments

--- selling-keywords/selling_keywords.php
+++ selling-keywords/selling_keywords.php
@@ -28,8 +28,8 @@ $array_useragents = [
 ];
 
 $array_cookies = [
-    'session=s%3Al8AxYb1GkJ5iVTXonunPZqrl8heCVkRQ.9yLYydFCvvOz5nJ1y4rqOJDTIl5zMdWp73rkPza9eg8',
-    'session=s%3A8FXrLwD1i8gqH2LbObNPFVF85-1bF_Fi.kdJfhAHaHSRLnTf8YEXyuI2GksFCJmmwy%2FAOayhOQ%2B8'
+    'session=s%3Al8AxYb1GkJ5iVTXonunPZqrl8heCVkRQ.9yLYydFCvvOz5nJ1y4rqOJDTIl5zMdWp73rkPza9eg8', // Elena
+    'session=s%3A8FXrLwD1i8gqH2LbObNPFVF85-1bF_Fi.kdJfhAHaHSRLnTf8YEXyuI2GksFCJmmwy%2FAOayhOQ%2B8' // Andrei
 ];
 
 
