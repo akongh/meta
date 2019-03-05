44a67269bf5cf22b5b68e6699cc15d4753dcd1fb
Andrei Korzhyts <andreikorzhyts@afoteris.com>
2019-03-05 18:31:24 +0300

    file_get_contents -> curl

--- selling_keywords/selling_keywords.php
+++ selling_keywords/selling_keywords.php
@@ -19,9 +19,6 @@ if ( isset( $_POST['keyword'] ) && $_POST['keyword'] != '' ) {
 
 $image_type = $_POST['imageType'];
 
-$array_works_data = ARRAY_WORKS_DATA( $autor, $keyword, $image_type );
-//echo( $array_works_data );
-
 $array_useragents = [
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.97 Safari/537.36 Vivaldi/1.9.818.49',
     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36 OPR/45.0.2552.812'
@@ -32,9 +29,18 @@ $array_cookies = [
     'session=s%3A8FXrLwD1i8gqH2LbObNPFVF85-1bF_Fi.kdJfhAHaHSRLnTf8YEXyuI2GksFCJmmwy%2FAOayhOQ%2B8' // Andrei
 ];
 
+$useragent = RANDOM_SELECT_USERAGENT( $array_useragents );
+$cookies   = RANDOM_SELECT_COOKIES( $array_cookies );
+
+if ( $autor == '' ) {
+    $search_url = 'https://www.shutterstock.com/search?searchterm=' . $keyword . '&image_type=' . $image_type . '&search_source=base_landing_page&language=en&page=1';
+} else {
+    $search_url = 'https://www.shutterstock.com/g/' . $autor . '?searchterm=' . $keyword . '&image_type=' . $image_type . '&search_source=base_gallery&language=en&sort=popular&safe=true';
+//        $search_url = 'https://www.shutterstock.com/g/' . $_PARAM_autor . '?search_source=base_gallery&language=en&sort=popular&safe=true';
+}
+
+$array_works_data       = ARRAY_WORKS_DATA( $search_url, $useragent, $cookies );
 $url                    = CREATE_URL( $array_works_data );
-$useragent              = RANDOM_SELECT_USERAGENT( $array_useragents );
-$cookies                = RANDOM_SELECT_COOKIES( $array_cookies );
 $json_selling_keywords  = GET_JSON_SELLING_KEYWORDS( $url, $useragent, $cookies );
 $array_selling_keywords = json_decode( $json_selling_keywords, true );
 
@@ -53,16 +59,16 @@ echo( json_encode( $array_works_data ) );
  * Functions.
  */
 
-function ARRAY_WORKS_DATA( $_PARAM_autor, $_PARAM_keyword, $_PARAM_image_type ) {
+function ARRAY_WORKS_DATA( $_PARAM_url, $_PARAM_useragent, $_PARAM_cookies ) {
 
-    if ( $_PARAM_autor == '' ) {
-        $search_url = 'https://www.shutterstock.com/search?searchterm=' . $_PARAM_keyword . '&image_type=' . $_PARAM_image_type . '&search_source=base_landing_page&language=en&page=1';
-    } else {
-        $search_url = 'https://www.shutterstock.com/g/' . $_PARAM_autor . '?searchterm=' . $_PARAM_keyword . '&image_type=' . $_PARAM_image_type . '&search_source=base_gallery&language=en&sort=popular&safe=true';
-//        $search_url = 'https://www.shutterstock.com/g/' . $_PARAM_autor . '?search_source=base_gallery&language=en&sort=popular&safe=true';
-    }
-
-    $data = file_get_contents( $search_url );
+    $SESSION = curl_init();
+    curl_setopt( $SESSION, CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $SESSION, CURLOPT_URL, $_PARAM_url );
+    curl_setopt( $SESSION, CURLOPT_USERAGENT, $_PARAM_useragent );
+    curl_setopt( $SESSION, CURLOPT_COOKIE, $_PARAM_cookies );
+    curl_setopt( $SESSION, CURLOPT_FOLLOWLOCATION, true );
+    $data = curl_exec( $SESSION );
+    curl_close( $SESSION );
 
     preg_match_all( '/(<li\ class="li js_item").*?(<\/li>)/su', $data, $array_works_block );
     if ( count( $array_works_block[0] ) == 0 ) {
