178359eef9757e9141153251b13452ce17bd57ed
Andrei Korzhyts <andreikorzhyts@afoteris.com>
2019-04-12 21:29:33 +0300

    Adding checking strings.

--- selling_keywords/selling_keywords.php
+++ selling_keywords/selling_keywords.php
@@ -36,10 +36,16 @@ $useragent = RANDOM_SELECT_STRING( $array_useragents );
 $cookies   = RANDOM_SELECT_STRING( $array_cookies );
 
 if ( $autor == '' ) {
-    $search_url       = 'https://www.shutterstock.com/en/search/' . $keyword . '?image_type=' . $image_type;
+    $search_url = 'https://www.shutterstock.com/en/search/' . $keyword . '?image_type=' . $image_type;
+
+//    echo $search_url;
+
     $array_works_data = ARRAY_WORKS_DATA( $search_url, $useragent, $cookies );
 } else {
-    $search_url       = 'https://www.shutterstock.com/g/' . $autor . '?searchterm=' . $keyword . '&search_source=base_gallery&language=en&page=1&sort=popular&image_type=' . $image_type . '&measurement=px&safe=true';
+    $search_url = 'https://www.shutterstock.com/g/' . $autor . '?searchterm=' . $keyword . '&search_source=base_gallery&language=en&page=1&sort=popular&image_type=' . $image_type . '&measurement=px&safe=true';
+
+//    echo $search_url;
+
     $array_works_data = ARRAY_WORKS_DATA_AUTHOR( $search_url, $useragent, $cookies );
 }
 
