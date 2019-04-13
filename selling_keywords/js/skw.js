fcf805f02b376a5ef7734701e3ed7fa6c504a4d4
Andrei Korzhyts <andreikorzhyts@afoteris.com>
2019-04-13 15:48:00 +0300

    Refactor

--- selling_keywords/js/skw.js
+++ selling_keywords/js/skw.js
@@ -34,7 +34,7 @@ clearKeywordButton.addEventListener("click", function (e) {
 }, false);
 createVariantsQueriesButton.addEventListener("click", function (e) {
     e.preventDefault();
-    createVariantsQueries('create_variants_queries_list.php');
+    createVariantsQueries('php/create_variants_queries_list.php');
 }, false);
 deleteVariantsQueriesButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -68,7 +68,7 @@ function getSellingKeywordsData() {
         function getWitsTimeout() {
             keyword = "keyword=" + encodeURIComponent(arrayKeywordsFromVariants[i]);
             sellingKeywordsRequest = keyword + '&' + imageType + '&' + autor;
-            sendPapamsGetSellingKeywords("selling_keywords.php", sellingKeywordsRequest, 1, arrayKeywordsFromVariants[i]);
+            sendPapamsGetSellingKeywords("php/create_array_works_data.php", sellingKeywordsRequest, 1, arrayKeywordsFromVariants[i]);
             i++;
             if (i < arrayKeywordsFromVariants.length) setTimeout(getWitsTimeout, 4000);
         }
@@ -77,7 +77,7 @@ function getSellingKeywordsData() {
     } else {
         keyword = "keyword=" + encodeURIComponent(document.querySelector('#keyword').value);
         sellingKeywordsRequest = keyword + '&' + imageType + '&' + autor;
-        sendPapamsGetSellingKeywords("selling_keywords.php", sellingKeywordsRequest, 0, document.querySelector('#keyword').value);
+        sendPapamsGetSellingKeywords("php/create_array_works_data.php", sellingKeywordsRequest, 0, document.querySelector('#keyword').value);
     }
 }
 
