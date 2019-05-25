dec72ad3df72d0ac3455fbffd66709f2aab8ffd4
Elena Abrazhevich <vaselena-info@mail.ru>
2019-05-26 00:22:00 +0300

    Закомментирована сессия Андрея.

--- selling_keywords/php/array_cookies.php
+++ selling_keywords/php/array_cookies.php
@@ -2,5 +2,5 @@
 
 $array_cookies = [
     'session=s%3Al8AxYb1GkJ5iVTXonunPZqrl8heCVkRQ.9yLYydFCvvOz5nJ1y4rqOJDTIl5zMdWp73rkPza9eg8', // Elena
-    'session=s%3AbMZgyZevBDVLVbPyN8ZUhaVVyHQTFSGY.DHrH7PHqfllY1TYEdaw3%2FrceKz2Nq65kMuIlGuIUUDU' // Andrei
+    //'session=s%3AbMZgyZevBDVLVbPyN8ZUhaVVyHQTFSGY.DHrH7PHqfllY1TYEdaw3%2FrceKz2Nq65kMuIlGuIUUDU' // Andrei
 ];
