b41c9064bb886105e654c5a435b60f923cca1054
Elena Abrazhevich <vaselena-info@mail.ru>
2019-05-27 19:30:12 +0300

    Обновлена сессия

--- selling_keywords/php/array_cookies.php
+++ selling_keywords/php/array_cookies.php
@@ -1,6 +1,6 @@
 <?php
 
 $array_cookies = [
-    'session=s%3Al8AxYb1GkJ5iVTXonunPZqrl8heCVkRQ.9yLYydFCvvOz5nJ1y4rqOJDTIl5zMdWp73rkPza9eg8', // Elena
+    'session=s%3AvlSGEiNtP4O09S4ZE4U8evm5fnt-irEj.zZShBmAM2LcZ%2FOAA3zXlEq%2FwejIOjgTqb3XoN2DDc24', // Elena
     //'session=s%3AbMZgyZevBDVLVbPyN8ZUhaVVyHQTFSGY.DHrH7PHqfllY1TYEdaw3%2FrceKz2Nq65kMuIlGuIUUDU' // Andrei
 ];
