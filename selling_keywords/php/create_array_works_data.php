c024194653251ccf582e15beaba7b274831ebc6b
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-03-17 00:43:05 +0300

    + //var_dump( $array_selling_keywords );

--- selling_keywords/php/create_array_works_data.php
+++ selling_keywords/php/create_array_works_data.php
@@ -57,6 +57,7 @@ $url                    = CREATE_URL( $array_works_data );
 $json_selling_keywords  = USE_CURL( $url, $useragent, $cookies );
 //echo $json_selling_keywords;
 $array_selling_keywords = json_decode( $json_selling_keywords, true );
+//var_dump( $array_selling_keywords );
 
 for ( $i = 0; $i < count( $array_works_data ); $i ++ ) {
     for ( $j = 0; $i < count( $array_selling_keywords ); $j ++ ) {
