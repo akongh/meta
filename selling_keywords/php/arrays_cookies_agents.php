e79bfb5d2df6bfca162940d38181e80cbbeeb1d0
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-03-17 01:32:54 +0300

    Обновлена session

--- selling_keywords/php/arrays_cookies_agents.php
+++ selling_keywords/php/arrays_cookies_agents.php
@@ -6,6 +6,6 @@ $array_useragents = [
 ];
 
 $array_cookies = [
-    'session=s%3AvlSGEiNtP4O09S4ZE4U8evm5fnt-irEj.zZShBmAM2LcZ%2FOAA3zXlEq%2FwejIOjgTqb3XoN2DDc24', // Elena
-    //'session=s%3AbMZgyZevBDVLVbPyN8ZUhaVVyHQTFSGY.DHrH7PHqfllY1TYEdaw3%2FrceKz2Nq65kMuIlGuIUUDU' // Andrei
+    //'session=s%3AvlSGEiNtP4O09S4ZE4U8evm5fnt-irEj.zZShBmAM2LcZ%2FOAA3zXlEq%2FwejIOjgTqb3XoN2DDc24', // Elena
+    'session=s%3AAfexAoKaLgP57j5zC-FAxkZdVtjuFoz3.sTp%2BnH5hMc3OygmiILjPh03pXyclwPx0QIHkMPqXwsM' // Andrei
 ];
