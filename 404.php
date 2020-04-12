0de0ff9a2dd1baad53bee626cbbf662cf134bb99
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-04-12 15:37:37 +0300

    Optimizing insertion of analytics code.

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php error_reporting( - 1 ); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>404</title>
+    <link href="/css/meta.css"
+          rel="stylesheet"
+          type="text/css">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/includes/yandex_metric_meta.php' );?>
+</head>
+<body>
+<div class="block-404-503">
+    <div class="content-404-503">
+        <a href="/meta.php" title="Вернуться из ниоткуда">404</a>
+    </div>
+</div>
+</body>
+</html>
---
+++ 503.php
@@ -0,0 +1,30 @@
+<?php error_reporting( - 1 ); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>503. Мета снова становится лучше.</title>
+    <link href="/css/meta.css"
+          rel="stylesheet"
+          type="text/css">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/includes/yandex_metric_meta.php' );?>
+</head>
+<body>
+<div class="block-404-503">
+    <div class="content-404-503">
+        <span class="bold">503</span>
+        <br>
+        <br>
+        Извините, если прервали ваш подбор.
+        <br>
+        Мета снова становится лучше.
+        <br>
+        Обычно это занимает не более часа.
+        <br>
+        <br>
+        <a href="/meta.php" title="Попробовать перейти на главную страницу Меты">Проверить включение Меты</a>
+    </div>
+</div>
+
+</body>
+</html>
