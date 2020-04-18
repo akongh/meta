18356ecfbe92c61c34ca25a1a2f0fcbaf1ecdf12
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-04-18 16:16:26 +0300

    refactoring DB connection

--- access_meta/db_connect.php
+++ access_meta/db_connect.php
@@ -2,17 +2,15 @@
 declare(strict_types=1);
 error_reporting(-1);
 
-$db_server   = "127.0.0.1";
-$db_user     = "root";
-$db_password = "root";
-$db_name     = "_meta";
+$server = "127.0.0.1";
+$user = "root";
+$password = "root";
+$name = "_meta";
 
-$mysqli = new mysqli($db_server, $db_user, $db_password, $db_name);
+$mysqli = new mysqli($server, $user, $password, $name);
 
 if ($mysqli->connect_errno) {
-    echo "<b>MySQL-server is not available…</b><br/>" . PHP_EOL .
-        $mysqli->connect_errno . "<br/>" . PHP_EOL .
-        $mysqli->connect_error;
+    echo $mysqli->errno . " -> " . $mysqli->error . PHP_EOL;
     exit;
 }
 
