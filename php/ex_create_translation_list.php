9ef99e4fce2338a92cac3a9f24360169e2b55af5
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-04-18 16:29:41 +0300

    refactoring mysql error messages

--- php/ex_create_translation_list.php
+++ php/ex_create_translation_list.php
@@ -16,13 +16,13 @@ $rus = $_POST['spisok_mesto'];
 for ( $i = 0; $i < count( $rus ); $i ++ ) {
 
     if (!($mysqli_stmt = $mysqli->prepare(SQL_P_Z))) {
-        echo $mysqli->errno . " -> " . $mysqli->error . PHP_EOL;
+        echo PHP_EOL . $mysqli->errno . " -> " . $mysqli->error . PHP_EOL;
     }
     if (!$mysqli_stmt->bind_param("s", $rus[ $i ])) {
-        echo $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
+        echo PHP_EOL . $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
     }
     if (!$mysqli_stmt->execute()) {
-        echo $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
+        echo PHP_EOL . $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
     }
     $result = $mysqli_stmt->get_result();
     $mysqli_stmt->close();
@@ -43,13 +43,13 @@ for ( $i = 0; $i < count( $rus ); $i ++ ) {
 
     //выясняем флаг русского слова, если оно уже есть в базе, или его отсутствие, если слова в базе пока нет
     if (!($mysqli_stmt = $mysqli->prepare(SQL_F))) {
-        echo $mysqli->errno . " -> " . $mysqli->error . PHP_EOL;
+        echo PHP_EOL . $mysqli->errno . " -> " . $mysqli->error . PHP_EOL;
     }
     if (!$mysqli_stmt->bind_param("s", $rus[ $i ])) {
-        echo $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
+        echo PHP_EOL . $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
     }
     if (!$mysqli_stmt->execute()) {
-        echo $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
+        echo PHP_EOL . $mysqli_stmt->errno . " -> " . $mysqli_stmt->error . PHP_EOL;
     }
     $result = $mysqli_stmt->get_result();
     $mysqli_stmt->close();
