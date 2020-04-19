3ceb072a4a026f4144a13be18191e86711cdac9d
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-04-19 23:36:07 +0300

    quotes

--- _privacy_path.php
+++ _privacy_path.php
@@ -2,4 +2,4 @@
 declare(strict_types=1);
 error_reporting(-1);
 
-require($_SERVER["DOCUMENT_ROOT"] . '/_meta_privacy/db_connection.php');
+require($_SERVER["DOCUMENT_ROOT"] . "/_meta_privacy/db_connection.php");
