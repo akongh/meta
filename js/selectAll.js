1da6e85e6b7fda73261159469aca8a88a266fb5e
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-04-23 20:45:58 +0300

    fix markup element name

--- js/selectAll.js
+++ js/selectAll.js
@@ -3,7 +3,7 @@ let title = document.getElementsByTagName("h1");
 title[0].addEventListener("click", selectAll);
 
 function selectAll() {
-    let keywordCheckbox = document.getElementsByName("slova_s_flagom[]");
+    let keywordCheckbox = document.getElementsByName("marked_kws[]");
     for (let i = 0; i < keywordCheckbox.length; i++) {
         keywordCheckbox[i].checked = true;
     }
