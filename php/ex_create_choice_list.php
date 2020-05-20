f725a94608b76a60b04193a57c53995bebe64fdd
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-20 12:11:08 +0300

    +todo

--- php/ex_create_choice_list.php
+++ php/ex_create_choice_list.php
@@ -82,7 +82,7 @@ if (isset($non_strict_choice) && $count_kws_query > 1) {
             if (count($arr_of_result) == $max_choice_amount) {
                 break;
             }
-            if (count($arr_of_result) > $max_choice_amount) {
+            if (count($arr_of_result) > $max_choice_amount) {//todo:уже в базе обрезается
                 $arr_of_result = array_slice($arr_of_result, 0, $max_choice_amount);
                 break;
             }
