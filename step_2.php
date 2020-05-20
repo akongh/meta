462424065149cbad090cab424d79497b83a1809b
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-20 20:18:08 +0300

    function instead markup

--- step_2.php
+++ step_2.php
@@ -81,7 +81,7 @@ var_dump($_SESSION);
         <br>
         <?php
         if (isset($_SESSION["err_msg_illegal_char"])) {
-            echo "<span class='error'>{$_SESSION["err_msg_illegal_char"]}</span><br>";
+            echo err_msg_markup($_SESSION["err_msg_illegal_char"]);
         }
         ?>
         <br>
