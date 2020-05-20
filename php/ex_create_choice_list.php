5a4739a91c27aaa6bf19c3a812f7f0f8178fdf23
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-20 16:00:17 +0300

    renaming of vars

--- php/ex_create_choice_list.php
+++ php/ex_create_choice_list.php
@@ -22,28 +22,28 @@ $max_choice_amount = $_POST["max_choice_amount"];
 $input_str_kws_query = $_POST["input_str_kws_query"];
 $input_str_kws_query = mb_strtolower(htmlspecialchars(strip_tags(stripslashes($input_str_kws_query))), "utf-8");
 $input_str_kws_query = preg_replace(["/ {2,}/", "/-{2,}/"], [" ", "-"], $input_str_kws_query);
-$kws_query = preg_split("/[\n,;]/", $input_str_kws_query, -1, PREG_SPLIT_NO_EMPTY);
+$arr_kws_query = preg_split("/[\n,;]/", $input_str_kws_query, -1, PREG_SPLIT_NO_EMPTY);
 
-foreach ($kws_query as &$value) {
+foreach ($arr_kws_query as &$value) {
     $value = trim($value);
 }
 unset($value);
 
-$kws_query = array_values(array_unique((array_diff($kws_query, array("")))));
-$_SESSION["kws_query"] = $kws_query;
+$arr_kws_query = array_values(array_unique((array_diff($arr_kws_query, array("")))));
+$_SESSION["arr_kws_query"] = $arr_kws_query;
 
-$count_kws_query = count($kws_query);
+$count_arr_kws_query = count($arr_kws_query);
 
 $err_mark = true;
-if (!isset($_SESSION["kws_state"]) && $kws_query == null) {
+if (!isset($_SESSION["kws_state"]) && $arr_kws_query == null) {
     $_SESSION["err_msg_empty_input"] = "Необходимы опорные ключевые слова.";
     $err_mark = false;
 }
-if ($count_kws_query > 8) {
+if ($count_arr_kws_query > 8) {
     $_SESSION["err_msg_illegal_kws_query_amount"] = "Не более 8-ми опорных ключевых слов.";
     $err_mark = false;
 }
-if ($count_kws_query > 0 && !preg_match($regex_check_ru_kws_query, implode("", $kws_query))) {
+if ($count_arr_kws_query > 0 && !preg_match($regex_check_ru_kws_query, implode("", $arr_kws_query))) {
     $_SESSION["err_msg_illegal_char"] = "Только кириллица, цифры, пробел и&nbsp;дефис.";
     $err_mark = false;
 }
@@ -52,13 +52,13 @@ if (false === $err_mark) {
     exit;
 }
 
-$str_kws_query = implode("','", $kws_query);
+$str_kws_query = implode("','", $arr_kws_query);
 
-if (isset($non_strict_choice) && $count_kws_query > 1) {
+if (isset($non_strict_choice) && $count_arr_kws_query > 1) {
     if (!($mysqli_stmt = $mysqli->prepare(sql_select_kws_choice($str_kws_query)))) {
         echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
     }
-    for ($i = $count_kws_query; $i > 0; $i--) {
+    for ($i = $count_arr_kws_query; $i > 0; $i--) {
         if (!$mysqli_stmt->bind_param("ii", $i, $max_choice_amount)) {
             echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
         }
@@ -74,7 +74,7 @@ if (isset($non_strict_choice) && $count_kws_query > 1) {
         $mysqli_stmt->free_result();
 
         if (isset($arr_of_result) && $arr_of_result != null) {
-            $arr_of_result = array_values(array_unique(array_merge($kws_query, $arr_of_result)));
+            $arr_of_result = array_values(array_unique(array_merge($arr_kws_query, $arr_of_result)));
 
             if ($i == 1) {
                 break;
@@ -84,7 +84,7 @@ if (isset($non_strict_choice) && $count_kws_query > 1) {
             }
         } else {
             if ($i == 1) {
-                $arr_of_result = $kws_query;
+                $arr_of_result = $arr_kws_query;
             }
         }
     }
@@ -94,7 +94,7 @@ if (isset($non_strict_choice) && $count_kws_query > 1) {
     if (!($mysqli_stmt = $mysqli->prepare(sql_select_kws_choice($str_kws_query)))) {
         echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->bind_param("ii", $count_kws_query, $max_choice_amount)) {
+    if (!$mysqli_stmt->bind_param("ii", $count_arr_kws_query, $max_choice_amount)) {
         echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
     }
     if (!$mysqli_stmt->execute()) {
@@ -110,9 +110,9 @@ if (isset($non_strict_choice) && $count_kws_query > 1) {
     $mysqli_stmt->close();
 
     if (isset($arr_of_result) && $arr_of_result != null) {
-        $arr_of_result = array_values(array_unique(array_merge($kws_query, $arr_of_result)));
+        $arr_of_result = array_values(array_unique(array_merge($arr_kws_query, $arr_of_result)));
     } else {
-        $arr_of_result = $kws_query;
+        $arr_of_result = $arr_kws_query;
     }
 }
 
