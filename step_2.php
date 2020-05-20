82d7c60f1d103df9c73b0dbd3423351e220daba2
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-20 17:16:56 +0300

    fix var name

--- step_2.php
+++ step_2.php
@@ -49,7 +49,7 @@ var_dump($_SESSION);
           action="/php/ex_create_check_choice_list.php">
         <?php
         foreach ($arr_kws_selection as $kw) {
-            if (in_array($kw, $_SESSION["kws_query"])) {
+            if (in_array($kw, $_SESSION["arr_kws_query"])) {
                 echo "
                     <label class='label-highlight'>
                     <input type='checkbox'
