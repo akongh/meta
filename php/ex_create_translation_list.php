a8dcbf5c4c526349852a7deba37ae6677fdbba70
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-22 00:53:27 +0300

    renaming, improving db request…

--- php/ex_create_translation_list.php
+++ php/ex_create_translation_list.php
@@ -7,28 +7,28 @@ session_start();
 require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
 require($_SERVER["DOCUMENT_ROOT"] . '/php/sql_prepared_statements.php');
 
-unset(
-    $_SESSION["err_msg_of_kws_amount"]
-);
+$arr_kws_ru = $_POST["arr_kws_marked"];
 
-$rus = $_POST["arr_kws_marked"];
-for ($i = 0; $i < count($rus); $i++) {
+if (!($mysqli_stmt_translation = $mysqli->prepare(SQL_SELECT_EN_TRANSLATION_AND_MEANING))) {
+    echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
+}
+if (!($mysqli_stmt_statuses = $mysqli->prepare(SQL_SELECT_KW_STATUSES))) {
+    echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
+}
 
-    if (!($mysqli_stmt = $mysqli->prepare(SQL_SELECT_EN_TRANSLATION_AND_MEANING))) {
-        echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
+for ($i = 0; $i < count($arr_kws_ru); $i++) {
+    if (!$mysqli_stmt_translation->bind_param("s", $arr_kws_ru[$i])) {
+        echo PHP_EOL . $mysqli_stmt_translation->errno . " --> " . $mysqli_stmt_translation->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->bind_param("s", $rus[$i])) {
-        echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
+    if (!$mysqli_stmt_translation->execute()) {
+        echo PHP_EOL . $mysqli_stmt_translation->errno . " --> " . $mysqli_stmt_translation->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->execute()) {
-        echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
-    }
-    $result = $mysqli_stmt->get_result();
+    $result = $mysqli_stmt_translation->get_result();
 
     $sql_select_en_translation_and_meaning = $result->fetch_all(MYSQLI_ASSOC);
 
-    $mysqli_stmt->free_result();
-    $mysqli_stmt->close();
+    $mysqli_stmt_translation->free_result();
+    $mysqli_stmt_translation->close();
 
     foreach ($sql_select_en_translation_and_meaning as $key => $val) {
         $p[$key] = $val["s"];
@@ -43,21 +43,18 @@ for ($i = 0; $i < count($rus); $i++) {
     }
 
     //выясняем флаг русского слова, если оно уже есть в базе, или его отсутствие, если слова в базе пока нет
-    if (!($mysqli_stmt = $mysqli->prepare(SQL_SELECT_KW_STATUSES))) {
-        echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
+    if (!$mysqli_stmt_statuses->bind_param("s", $arr_kws_ru[$i])) {
+        echo PHP_EOL . $mysqli_stmt_statuses->errno . " --> " . $mysqli_stmt_statuses->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->bind_param("s", $rus[$i])) {
-        echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
+    if (!$mysqli_stmt_statuses->execute()) {
+        echo PHP_EOL . $mysqli_stmt_statuses->errno . " --> " . $mysqli_stmt_statuses->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->execute()) {
-        echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
-    }
-    $result = $mysqli_stmt->get_result();
+    $result = $mysqli_stmt_statuses->get_result();
 
     $sql_select_kw_statuses = $result->fetch_all(MYSQLI_ASSOC);
 
-    $mysqli_stmt->free_result();
-    $mysqli_stmt->close();
+    $mysqli_stmt_statuses->free_result();
+    $mysqli_stmt_statuses->close();
 
     foreach ($sql_select_kw_statuses as $key => $val) {
         $f[$key] = $val["f"];
@@ -74,7 +71,7 @@ for ($i = 0; $i < count($rus); $i++) {
         $with_translation[$i] = "
         <div class = 'block-translated'>
 		    <span class = 'keyword-ru'>
-		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $rus[$i] . "'>" . $rus[$i] . "
+		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $arr_kws_ru[$i] . "'>" . $arr_kws_ru[$i] . "
 		    </span>
 		    <br>
 		    " . $p_z . "
@@ -92,7 +89,7 @@ for ($i = 0; $i < count($rus); $i++) {
             $with_translation[$i] = "
         <div class = 'block-translated'>
 		    <span class = 'keyword-ru'>
-		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $rus[$i] . "'>" . $rus[$i] . "
+		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $arr_kws_ru[$i] . "'>" . $arr_kws_ru[$i] . "
 		    </span>
 		    <br>
 		    " . $p_z . "
@@ -101,27 +98,27 @@ for ($i = 0; $i < count($rus); $i++) {
 
         } else {
             if (!isset($p_z) && ($f == 0 or $f == null)) {
-                $neperevedennye[$i] = $rus[$i];
+                $neperevedennye[$i] = $arr_kws_ru[$i];
                 $with_translation[$i] = "
         <div class = 'block-not-translated'>
 		    <span class = 'keyword-ru'>
-		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $rus[$i] . "'>" . $rus[$i] . "
+		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $arr_kws_ru[$i] . "'>" . $arr_kws_ru[$i] . "
 		    </span>
 		    <br>
-		    (перевода пока нет)<input type='checkbox' name='zayavka[]' class='hidden' checked value = '" . $rus[$i] . "'>
+		    (перевода пока нет)<input type='checkbox' name='zayavka[]' class='hidden' checked value = '" . $arr_kws_ru[$i] . "'>
 		</div>
 		";
 
             } else {
                 if (!isset($p_z) && $f == 7) {
-                    $neperevedennye[$i] = $rus[$i];
+                    $neperevedennye[$i] = $arr_kws_ru[$i];
                     $with_translation[$i] = "
         <div class = 'block-not-translated'>
 		    <span class = 'keyword-ru'>
-		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $rus[$i] . "'>" . $rus[$i] . "
+		        <input type='checkbox' name='russk[]' class='hidden' checked value = '" . $arr_kws_ru[$i] . "'>" . $arr_kws_ru[$i] . "
 		    </span>
 		    <br>
-		    (в заявке на перевод)<input type='checkbox' name='zayavka[]' class='hidden' checked value = '" . $rus[$i] . "'>
+		    (в заявке на перевод)<input type='checkbox' name='zayavka[]' class='hidden' checked value = '" . $arr_kws_ru[$i] . "'>
 		</div>
 		";
                 }
