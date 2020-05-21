790ac027ac9e0c9c6ba9448fdd87d56337f8a66f
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-21 21:36:43 +0300

    removing of unnecessary "require"

--- php/ex_create_ordering_list.php
+++ php/ex_create_ordering_list.php
@@ -5,7 +5,6 @@ error_reporting(-1);
 session_start();
 
 require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
-require($_SERVER["DOCUMENT_ROOT"] . '/php/sql_prepared_statements.php');
 
 if (!isset($_POST["arr_kws_marked"]) || count($_POST["arr_kws_marked"]) < 8) {
     $_SESSION["err_msg_of_kws_amount"] = "В наборе менее 8-ми уникальных ключевых слов.";
