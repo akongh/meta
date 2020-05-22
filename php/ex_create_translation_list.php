2b9b676f2030524606df37a967d0b5650be994c6
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-23 01:34:41 +0300

    array of kws list with translations + status

--- php/ex_create_translation_list.php
+++ php/ex_create_translation_list.php
@@ -12,8 +12,13 @@ $_SESSION["arr_kws_ordered"] = $_POST["arr_kws_marked"];
 if (!($mysqli_stmt_translation = $mysqli->prepare(SQL_SELECT_EN_TRANSLATION_AND_MEANING))) {
     echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
 }
+if (!($mysqli_stmt_statuses = $mysqli->prepare(SQL_SELECT_KW_STATUSES))) {
+    echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
+}
 
 foreach ($_SESSION["arr_kws_ordered"] as $kw) {
+    $arr_kw_translations[0] = $kw;
+
     if (!$mysqli_stmt_translation->bind_param("s", $kw)) {
         echo PHP_EOL . $mysqli_stmt_translation->errno . " --> " . $mysqli_stmt_translation->error . PHP_EOL;
     }
@@ -21,10 +26,20 @@ foreach ($_SESSION["arr_kws_ordered"] as $kw) {
         echo PHP_EOL . $mysqli_stmt_translation->errno . " --> " . $mysqli_stmt_translation->error . PHP_EOL;
     }
     $result = $mysqli_stmt_translation->get_result();
-    $arr_kw_translations[0] = $kw;
     $arr_kw_translations[1] = $result->fetch_all(MYSQLI_ASSOC);
-    $arr_list_kws_translations[] = $arr_kw_translations;
     $mysqli_stmt_translation->free_result();
+
+    if (!$mysqli_stmt_statuses->bind_param("s", $kw)) {
+        echo PHP_EOL . $mysqli_stmt_statuses->errno . " --> " . $mysqli_stmt_statuses->error . PHP_EOL;
+    }
+    if (!$mysqli_stmt_statuses->execute()) {
+        echo PHP_EOL . $mysqli_stmt_statuses->errno . " --> " . $mysqli_stmt_statuses->error . PHP_EOL;
+    }
+    $result = $mysqli_stmt_statuses->get_result();
+    $arr_kw_translations[2] = $result->fetch_all(MYSQLI_ASSOC)[0]["f"];
+    $mysqli_stmt_statuses->free_result();
+
+    $arr_list_kws_translations[] = $arr_kw_translations;
 }
 
 $_SESSION["arr_list_kws_translations"] = $arr_list_kws_translations;
