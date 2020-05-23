a18d6e5298e12f9f88c3de1510256dc068e736aa
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-23 16:47:50 +0300

    deep refactoring

--- php/ex_create_results_choice.php
+++ php/ex_create_results_choice.php
@@ -7,25 +7,8 @@ require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
 require($_SERVER["DOCUMENT_ROOT"] . '/php/sql_prepared_statements.php');
 
 if (isset($_POST["russk"])) {
-    $kws_ru = array_values(array_unique($_POST["russk"]));
-}
-if (isset($_POST["angl"])) {
-    $kws_en = array_values(array_unique($_POST["angl"]));
-}
-if (isset($_POST["zayavka"])) {
-    $kws_mark_transl = $_POST["zayavka"];
-}
-
-if (isset($kws_ru)) {
-    $_SESSION["amount_chosen_ru_kws"] = count($kws_ru);
-    $_SESSION["resulting_ru_kws_set"] = implode(", ", $kws_ru);
-
+    $_SESSION["arr_kws_ru"] = $_POST["russk"];
     $kwsset_time = time();
-    $kws_ru_to_db = $kws_ru;
-
-    for ($i = 0; $i < count($kws_ru_to_db); $i++) {
-        $kws_ru_to_db[$i] = preg_replace(["/ {2,}/", "/'/"], [" ", "\'"], trim($kws_ru_to_db[$i]));//todo: is it necessary "/ {2,}/" --> " " ?
-    }
 
     // Создание номера нового набора todo:transl
 
@@ -43,17 +26,19 @@ if (isset($kws_ru)) {
 
     // Добавление новых ключевых слов в БД todo:transl
 
+    foreach ($_SESSION["arr_kws_ru"] as $kw_ru) {
+        $arr_kws_ru_to_db[] = preg_replace(["/ {2,}/", "/'/"], [" ", "\'"], trim($kw_ru));//todo: is it necessary "/ {2,}/" --> " " ?
+    }
+    $str_kws_ru_to_db = implode("','", $arr_kws_ru_to_db);
+
     if (!($mysqli_stmt = $mysqli->prepare(SQL_INSERT_CREATE_KWS_SET_KWS))) {
         echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->bind_param("s", $kw_ru_to_db)) {
+    if (!$mysqli_stmt->bind_param("s", $str_kws_ru_to_db)) {
         echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
     }
-    for ($i = 0; $i < count($kws_ru_to_db); $i++) {
-        $kw_ru_to_db = $kws_ru_to_db[$i];
-        if (!$mysqli_stmt->execute()) {
-            echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
-        }
+    if (!$mysqli_stmt->execute()) {
+        echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
     }
 
     // Создание связей ключевых слов с набором todo:transl
@@ -61,24 +46,23 @@ if (isset($kws_ru)) {
     if (!($mysqli_stmt = $mysqli->prepare(SQL_INSERT_CREATE_KWS_SET_RELATIONS))) {
         echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
     }
-    if (!$mysqli_stmt->bind_param("is", $kwsset_id, $kw_ru_to_db)) {
-        echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
-    }
-    for ($i = 0; $i < count($kws_ru_to_db); $i++) {
-        $kw_ru_to_db = $kws_ru_to_db[$i];
+    foreach ($arr_kws_ru_to_db as $kw_ru_to_db) {
+        if (!$mysqli_stmt->bind_param("is", $kwsset_id, $kw_ru_to_db)) {
+            echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
+        }
         if (!$mysqli_stmt->execute()) {
             echo PHP_EOL . $mysqli_stmt->errno . " --> " . $mysqli_stmt->error . PHP_EOL;
         }
     }
 }
-if (isset($kws_en)) {
-    $_SESSION["amount_chosen_en_kws"] = count($kws_en);
-    $_SESSION["resulting_en_kws_set"] = implode(", ", $kws_en);
-} else {
-    $_SESSION["amount_chosen_en_kws"] = 0;
+
+if (isset($_POST["angl"])) {
+    $_SESSION["arr_kws_en"] = $_POST["angl"];
 }
-if (isset($kws_mark_transl)) {
-    $kws_mark_transl = implode("','", $kws_mark_transl);
+
+if (isset($_POST["zayavka"])) {
+    $_SESSION["arr_kws_untranslated"] = $_POST["zayavka"];
+    $kws_mark_transl = implode("','", $_SESSION["arr_kws_untranslated"]);
     $mysqli->query(sql_update_mark_kws_for_translation($kws_mark_transl));
 }
 
