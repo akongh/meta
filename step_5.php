55177beb43c8888487255e21c9a8501e13489476
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-23 13:06:12 +0300

    creating of markup translation list (broken)

--- step_5.php
+++ step_5.php
@@ -40,158 +40,57 @@ var_dump($_SESSION);
     <form method="post"
           action="/php/ex_create_results_choice.php">
 
-        <?php if (!($mysqli_stmt_translation = $mysqli->prepare(SQL_SELECT_EN_TRANSLATION_AND_MEANING))) {
-            echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
-        }
-        if (!($mysqli_stmt_statuses = $mysqli->prepare(SQL_SELECT_KW_STATUSES))) {
-            echo PHP_EOL . $mysqli->errno . " --> " . $mysqli->error . PHP_EOL;
-        }
-
-        for ($i = 0; $i < count($_SESSION["arr_kws_ordered"]); $i++) {
-            if (!$mysqli_stmt_translation->bind_param("s", $_SESSION["arr_kws_ordered"][$i])) {
-                echo PHP_EOL . $mysqli_stmt_translation->errno . " --> " . $mysqli_stmt_translation->error . PHP_EOL;
-            }
-            if (!$mysqli_stmt_translation->execute()) {
-                echo PHP_EOL . $mysqli_stmt_translation->errno . " --> " . $mysqli_stmt_translation->error . PHP_EOL;
-            }
-            $result = $mysqli_stmt_translation->get_result();
-            $arr_en_translation_meaning = $result->fetch_all(MYSQLI_ASSOC);
-            $mysqli_stmt_translation->free_result();
-
-            foreach ($arr_en_translation_meaning as $key => $val) {
-                $translation[$key] = $val["s"];
-                $translation_entity[$key] = preg_replace("/'/", "&#039;", $translation[$key]);
-                $meaning[$key] = $val["z"];
-                $translation_meaning[$key] = "
-        <label class='label-highlight separate-checkbox'>
-            <span class='keyword-en'>
-                <input type='checkbox'
-                       name='angl[]'
-                       value='" . $translation_entity[$key] . "'> " . $translation[$key] . "
-            </span> — " . $meaning[$key] . "
-        </label>
-        ";
-            }
-
-            //выясняем флаг русского слова, если оно уже есть в базе, или его отсутствие, если слова в базе пока нет
-            if (!$mysqli_stmt_statuses->bind_param("s", $_SESSION["arr_kws_ordered"][$i])) {
-                echo PHP_EOL . $mysqli_stmt_statuses->errno . " --> " . $mysqli_stmt_statuses->error . PHP_EOL;
-            }
-            if (!$mysqli_stmt_statuses->execute()) {
-                echo PHP_EOL . $mysqli_stmt_statuses->errno . " --> " . $mysqli_stmt_statuses->error . PHP_EOL;
-            }
-            $result = $mysqli_stmt_statuses->get_result();
-            $kw_status = $result->fetch_all(MYSQLI_ASSOC);
-            $mysqli_stmt_statuses->free_result();
-
-            foreach ($kw_status as $key => $val) {
-                $flag[$key] = $val["f"];
-            }
-
-            if (isset($flag[0])) {
-                $flag = $flag[0];
+        <?php
+        foreach ($_SESSION["arr_list_kws_translations"] as $el) {
+            if ($el[2] == 0) {
+                $comment = "(перевода пока нет)";
             } else {
-                $flag = null;
+                if ($el[2] == 7) {
+                    $comment = "(в заявке на перевод)";
+                }
             }
 
-            if (isset($translation_meaning) && count($translation_meaning) > 1) {
-                $translation_meaning = implode("\n", $translation_meaning);
-                $arr_markup_translation_block[$i] = "
-        <div class='block-translated'>
-		    <span class='keyword-ru'>
-		        <input type='checkbox'
-                       name='russk[]'
-                       class='hidden'
-                       checked
-                       value='" . $_SESSION["arr_kws_ordered"][$i] . "'>" . $_SESSION["arr_kws_ordered"][$i] . "
-		    </span>
-            <br>
-            " . $translation_meaning . "
-        </div>
-        ";
-            } else {
-                if (isset($translation_meaning) && count($translation_meaning) == 1) {
-                    $translation_meaning = "
-        <label class='label-highlight separate-checkbox'>
-                <span class='keyword-en'>
+            if ($el[2] == 0 or $el[2] == 7) {
+                $div_class = "block-not-translated";
+                $translation_meaning_markup = $comment . "
                     <input type='checkbox'
-                           name='angl[]'
-                           checked
-                           value='" . $translation_entity[0] . "'> " . $translation[0] . "
-                </span> — " . $meaning[0] . "
-        </label>";
-                    $arr_markup_translation_block[$i] = "
-        <div class='block-translated'>
-                <span class='keyword-ru'>
-                    <input type='checkbox'
-                           name='russk[]'
+                           name='zayavka[]'
                            class='hidden'
                            checked
-                           value='" . $_SESSION["arr_kws_ordered"][$i] . "'>" . $_SESSION["arr_kws_ordered"][$i] . "
-                </span>
-            <br>
-            " . $translation_meaning . "
-        </div>
-        ";
-                } else {
-                    if (!isset($translation_meaning) && ($flag == 0 or $flag == null)) {
-                        $arr_kws_untranslated[$i] = $_SESSION["arr_kws_ordered"][$i];
-                        $arr_markup_translation_block[$i] = "
-        <div class='block-not-translated'>
+                           value='{$el[0]}'>
+                           ";
+            } else {
+                $div_class = "block-translated";
+                foreach ($el[1] as $val) {
+                    $translation = $val["s"];
+                    $translation_entity = preg_replace("/'/", "&#039;", $translation);
+                    $meaning = $val["z"];
+                    $translation_meaning[] = "
+                        <label class='label-highlight separate-checkbox'>
+                            <span class='keyword-en'>
+                                <input type='checkbox'
+                                       name='angl[]'
+                                       value='{$translation_entity}'>{$translation}
+                            </span> — {$meaning}
+                        </label>
+                        ";
+                }
+                $translation_meaning_markup = implode("\n", $translation_meaning);
+            }
+        }
+
+        echo "<div class='{$div_class}'>
                     <span class='keyword-ru'>
                         <input type='checkbox'
                                name='russk[]'
                                class='hidden'
                                checked
-                               value='" . $_SESSION["arr_kws_ordered"][$i] . "'>" . $_SESSION["arr_kws_ordered"][$i] . "
-                    </span>
-            <br>
-            (перевода пока нет)<input type='checkbox'
-                                      name='zayavka[]'
-                                      class='hidden'
-                                      checked
-                                      value='" . $_SESSION["arr_kws_ordered"][$i] . "'>
-        </div>
-        ";
-                    } else {
-                        if (!isset($translation_meaning) && $flag == 7) {
-                            $arr_kws_untranslated[$i] = $_SESSION["arr_kws_ordered"][$i];
-                            $arr_markup_translation_block[$i] = "
-        <div class='block-not-translated'>
-                        <span class='keyword-ru'>
-                            <input type='checkbox'
-                                   name='russk[]'
-                                   class='hidden'
-                                   checked
-                                   value='" . $_SESSION["arr_kws_ordered"][$i] . "'>" . $_SESSION["arr_kws_ordered"][$i] . "
-                        </span>
-            <br>
-            (в заявке на перевод)<input type='checkbox'
-                                        name='zayavka[]'
-                                        class='hidden'
-                                        checked
-                                        value='" . $_SESSION["arr_kws_ordered"][$i] . "'>
-        </div>
-        ";
-                        }
-                    }
-                }
-            }
-
-            unset($translation_meaning, $translation, $meaning, $flag);
-        }
-
-        $mysqli_stmt_translation->close();
-        $mysqli_stmt_statuses->close();
-
-        if (isset($arr_kws_untranslated)) {
-            $_SESSION["arr_kws_untranslated"] = $arr_kws_untranslated;
-        }
-
-        echo implode("\n", $arr_markup_translation_block);
-
-        $mysqli->close(); ?>
-
+                               value='{$el[0]}'>{$el[0]}</span>
+                    <br>
+                    {$translation_meaning_markup}
+              </div>
+              ";
+        ?>
         <br>
         <br>
         <span class="counter">
