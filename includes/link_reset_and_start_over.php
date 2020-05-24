d6c2f82934f53ff675540e779cd0081033ba6b46
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-24 18:57:11 +0300

    renaming

--- includes/link_reset_and_start_over.php
+++ includes/link_reset_and_start_over.php
@@ -1,4 +1,4 @@
 <br>
 <a class="link-button-reset"
-   href="/reset_and_start_over.php"
+   href="/step_any_1.php"
    title="Сбросить текущий подбор и перейти на первый шаг">[Сбросить и сначала]</a>
