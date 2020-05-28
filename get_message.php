6c1ae8fe883ebc64623ecbb8fd26634fd4de9d35
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-05-28 14:22:20 +0300

    fix & simplify of sending of mail

--- get_message.php
+++ get_message.php
@@ -3,15 +3,11 @@ declare(strict_types=1);
 error_reporting(-1);
 
 if (isset($_POST["messageText"])) {
-    $e_mail = "andreikorzhyts@google.com";
+    $e_mail = "andreikorzhyts@gmail.com";
     $subject = "META.afoteris.com";
     $message_text = $_POST["messageText"];
-    $headers = array(
-        'From' => 'META.afoteris.com',
-        'X-Mailer' => 'PHP/' . phpversion()
-    );
 
-    if (mail($e_mail, $subject, $message_text, $headers)) {
+    if (mail($e_mail, $subject, $message_text)) {
         echo("Сообщение отправлено.");
     } else {
         echo("Что-то не так… Сообщение не отправлено. Попробуйте andreikorzhyts на gmail.");
