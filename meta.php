7cfa2de48529df6b9da4a7927e1e5a6387d46715
Elena Abrazhevich <vaselena-info@mail.ru>
2020-06-01 22:09:46 +0300

    design

--- meta.php
+++ meta.php
@@ -10,12 +10,12 @@ require($_SERVER["DOCUMENT_ROOT"] . "/_privacy_path.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/sql_prepared_statements.php");
 
 $mysqli_result = $mysqli->query(SQL_SELECT_COUNT_TRANSLATED_KWS);
-$arr_result = $mysqli_result->fetch_array();
+$arr_result    = $mysqli_result->fetch_array();
 $mysqli_result->free();
 $count_translated = number_format((float)$arr_result[0], 0, "", "&nbsp;");
 
 $mysqli_result = $mysqli->query(SQL_SELECT_COUNT_KWS_FOR_TRANSLATION);
-$arr_result = $mysqli_result->fetch_array();
+$arr_result    = $mysqli_result->fetch_array();
 $mysqli_result->free();
 $count_request = number_format((float)$arr_result[0], 0, "", "&nbsp;");
 
@@ -43,19 +43,21 @@ $mysqli->close();
 <div class="wrap">
     <h1>МЕТА</h1>
     <h2>Русско-английский сервис подбора ключевых слов для&nbsp;стоков</h2>
-    <p>
-        Ключевых слов переведено
-        <span class="amount"><?php if (isset($count_translated)) {
-                echo "{$count_translated}";
-            }; ?></span>
-    </p>
-    <p>
-        В <a href="/queue_for_translation.php"
-             title="Список ключевых слов, добавленных пользователями в очередь на перевод">очереди на перевод</a>
-        <span class="amount"><?php if (isset($count_request)) {
-                echo $count_request;
-            }; ?></span>
-    </p>
+    <div class="meta_info">
+        <p>
+            Ключевых слов переведено
+            <span class="amount"><?php if (isset($count_translated)) {
+                    echo "{$count_translated}";
+                }; ?></span>
+        </p>
+        <p>
+            В <a href="/queue_for_translation.php"
+                 title="Список ключевых слов, добавленных пользователями в очередь на перевод">очереди на перевод</a>
+            <span class="amount"><?php if (isset($count_request)) {
+                    echo $count_request;
+                }; ?></span>
+        </p>
+    </div>
     <div class="content_right">
         <a href="/step_1.php"
            title="Начать подбирать ключевые слова">[Русско-английский подбор]</a>
