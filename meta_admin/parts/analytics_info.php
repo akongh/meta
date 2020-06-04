bc5844e050fb61bbff6d5dc316d92b406b097486
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-04 16:29:05 +0300

    renaming

--- meta_admin/parts/analytics_info.php
+++ meta_admin/parts/analytics_info.php
@@ -1,4 +1,4 @@
-<div class="yandex_metric_admin">
+<div class="analytics_info">
     <!-- Yandex.Metrika informer -->
     <a href="https://metrika.yandex.by/stat/?id=55997491&amp;from=informer"
        target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/55997491/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
