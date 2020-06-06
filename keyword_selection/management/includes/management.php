1f91643b9889e55a0587cf954147511b31f2a320
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-06 15:49:34 +0300

    renaming

--- keyword_selection/management/includes/management.php
+++ keyword_selection/management/includes/management.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>Управление Метой</title>
+    <title>Meta management</title>
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"
