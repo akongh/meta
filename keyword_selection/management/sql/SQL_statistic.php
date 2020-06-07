a2527dacbc83b27e5a71fc0c42840a460ae7393a
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-07 13:32:32 +0300

    reformat

--- keyword_selection/management/sql/SQL_statistic.php
+++ keyword_selection/management/sql/SQL_statistic.php
@@ -1,9 +1,9 @@
 <?php
 
-$amount_ru_kws_zapros     = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `k-ts`" );
-$amount_ru_kws_set_zapros  = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `k-tn`" );
-$amount_en_kws_zapros     = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `l-ts`" );
-$na_zayavke_zapros       = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `k-ts` WHERE `f` = '7'" );
-$perevedeno_zapros       = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `k-ts` WHERE `f` = '1'" );
-$hint_translation_querry = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `l-ts` WHERE `f` = '1'" );
-$hint_request_querry     = mysqli_query( $mysqli, "SELECT COUNT(*) FROM `l-ts` WHERE `f` = '7'" );
+$amount_ru_kws_zapros = mysqli_query($mysqli, "SELECT COUNT(*) FROM `k-ts`");
+$amount_ru_kws_set_zapros = mysqli_query($mysqli, "SELECT COUNT(*) FROM `k-tn`");
+$amount_en_kws_zapros = mysqli_query($mysqli, "SELECT COUNT(*) FROM `l-ts`");
+$na_zayavke_zapros = mysqli_query($mysqli, "SELECT COUNT(*) FROM `k-ts` WHERE `f` = '7'");
+$perevedeno_zapros = mysqli_query($mysqli, "SELECT COUNT(*) FROM `k-ts` WHERE `f` = '1'");
+$hint_translation_querry = mysqli_query($mysqli, "SELECT COUNT(*) FROM `l-ts` WHERE `f` = '1'");
+$hint_request_querry = mysqli_query($mysqli, "SELECT COUNT(*) FROM `l-ts` WHERE `f` = '7'");
