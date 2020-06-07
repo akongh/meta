69b6a778ba92ca352f860bcb8bea9eb2f4d43a13
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-07 17:16:01 +0300

    markup

--- search_hints/search_hints.php
+++ search_hints/search_hints.php
@@ -33,13 +33,15 @@ require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
     <?php
     require($_SERVER["DOCUMENT_ROOT"] . "/link_to_index.php"); ?>
     <h1>Поисковые подсказки</h1>
+    <label for="in-russian">Вспомогательный русско-английский перевод</label>
     <input type="text" id="in-russian" class="textarea-keywords" maxlength="64">
     <span id='error-translations' class='error'></span>
     <div class="content-right">
-        <a href="##" id="get-translation-button" class="link-button" title="Получить перевод">[Перевод]</a>
-        <a href="##" id="clear-translation-button" class="link-button" title="Очистить перевод">[х]</a>
+        <a href="#" id="get-translation-button" class="link-button" title="Получить перевод">[Перевод]</a>
+        <a href="#" id="clear-translation-button" class="link-button" title="Очистить перевод">[х]</a>
     </div>
     <div id="translations-area">Список перевода пуст.</div>
+    <label for="basic-keywords-string">Опорные ключевые слова</label>
     <textarea id="basic-keywords-string"
               name="basic-keywords-string"
               class="textarea-keywords"
@@ -49,8 +51,8 @@ require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
               autofocus></textarea>
     <span id="error-hints" class='error'></span>
     <div class="content-right">
-        <a href="##" id="add-keywords-to-list-button" class="link-button" title="Добавить в список свои ключевые слова">[Добавить в&nbsp;список]</a>
-        <a href="##" id="clear-button" class="link-button" title="Очистить поле запроса">[x]</a>
+        <a href="#" id="add-keywords-to-list-button" class="link-button" title="Добавить в список свои ключевые слова">[Добавить в&nbsp;список]</a>
+        <a href="#" id="clear-button" class="link-button" title="Очистить поле запроса">[x]</a>
     </div>
     <label><input type="radio" name="media-type" value="image" checked> Изображения</label>
     <label><input type="radio" name="media-type" value="audio"> Аудио</label>
@@ -64,17 +66,17 @@ require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
     <input id="get-basic-keywords-button-bigstockphoto" type="submit" value="От Бигстокфото">
     <input id="get-basic-keywords-button-depositphotos" type="submit" value="От Депозитфотос">
     <input id="get-basic-keywords-button-123rf" type="submit" value="От 123РФ">
-    <a name="top"></a>
-    <a href="##" id="create-result-string-button" class="link-button" title="Создать строку результата">[Результат]</a>
-    <a href="##" id="rank-hints-list-button" class="link-button" title="Задать очерёдность подсказок в списке">[Очерёдность]</a>
-    <a href="##" id="return-to-list-view-button" class="link-button" title="Вернуть список подсказок">[Список]</a>
-    <a href="##" id="sort-a-z-button" class="link-button" title="Сортировать список по алфавиту">[По&nbsp;алфавиту]</a>
-    <a href="##" id="delete-deselected-hints-button" class="link-button" title="Очистить список от невыбранных подсказок">[Очистить]</a>
-    <a href="##" id="select-all-hints-button" class="link-button" title="Вернуть список подсказок">[Все]</a>
-    <a href="##" id="deselect-all-hints-button" class="link-button" title="Очистить список от невыбранных подсказок">[Ничего]</a>
+    <a id="top"></a>
+    <a href="#" id="create-result-string-button" class="link-button" title="Создать строку результата">[Результат]</a>
+    <a href="#" id="rank-hints-list-button" class="link-button" title="Задать очерёдность подсказок в списке">[Очерёдность]</a>
+    <a href="#" id="return-to-list-view-button" class="link-button" title="Вернуть список подсказок">[Список]</a>
+    <a href="#" id="sort-a-z-button" class="link-button" title="Сортировать список по алфавиту">[По&nbsp;алфавиту]</a>
+    <a href="#" id="delete-deselected-hints-button" class="link-button" title="Очистить список от невыбранных подсказок">[Очистить]</a>
+    <a href="#" id="select-all-hints-button" class="link-button" title="Вернуть список подсказок">[Все]</a>
+    <a href="#" id="deselect-all-hints-button" class="link-button" title="Очистить список от невыбранных подсказок">[Ничего]</a>
     <div class="content-right">
         <span id="hints-total-and-selected" class="counter"></span>
-        <a href="##" id="delete-hints-objects-array-button" class="link-button" title="Удалить текущий список подсказок">[x]</a>
+        <a href="#" id="delete-hints-objects-array-button" class="link-button" title="Удалить текущий список подсказок">[x]</a>
     </div>
     <div id="hints-area">Список подсказок пуст.</div>
     <?php
