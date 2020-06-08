88594f7b5c9ceb8fd315740b8949753119fbaeff
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-08 11:39:59 +0300

    refactor, reformat…

--- search_hints/php/ex_add_hints_to_translation.php
+++ search_hints/php/ex_add_hints_to_translation.php
@@ -3,22 +3,22 @@
 declare(strict_types=1);
 error_reporting(-1);
 
+require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
+
 $json_hints_string_for_translation = $_POST["jsonHintsStringForTranlation"];
 $json_hints_array_for_translation = json_decode($json_hints_string_for_translation, JSON_UNESCAPED_UNICODE);
 
-require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
-for ($i = 0; $i < count($json_hints_array_for_translation); $i++) {
-    $json_hints_array_for_translation[$i] = preg_replace("/&amp;/", "&", $json_hints_array_for_translation[$i]);
+foreach ($json_hints_array_for_translation as $element) {
+    $element = preg_replace(["/&amp;/", "/'/"], ["&", "\'"], $element);
 
     mysqli_query($mysqli, "
 	INSERT IGNORE INTO `l-ts` (`s`)
-	VALUES ('" . preg_replace("/'/", "\'", $json_hints_array_for_translation[$i]) . "')
+	VALUES ('" . $element . "')
     ");
-
     mysqli_query($mysqli, "
 	UPDATE `l-ts`
 	SET `f` = 7
-	WHERE `s` = '" . preg_replace("/'/", "\'", $json_hints_array_for_translation[$i]) . "' and `f` = 0
+	WHERE `s` = '" . $element . "' and `f` = 0
     ");
 }
 
