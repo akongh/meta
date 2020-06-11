99f7de183541263ce1e0ae6fcb148bb11be6f8dd
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-11 10:42:06 +0300

    search-hints clearing

--- search_hints/search_hints.php
+++ search_hints/search_hints.php
@@ -82,10 +82,6 @@ require($_SERVER["DOCUMENT_ROOT"] . '/_privacy_path.php');
            id="get-basic-keywords-button-123rf"
            value="От 123РФ">
     <a id="top"></a>
-<!--    <a href="#"-->
-<!--       id="create-result-string-button"-->
-<!--       class="link-button"-->
-<!--       title="Создать строку результата из списка ключевых слов">[Результат]</a>-->
     <a href="#"
        id="rank-hints-list-button"
        class="link-button"
