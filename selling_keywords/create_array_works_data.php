f0040cce43381230fe330790f89a15b45865ea6c
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-13 11:08:07 +0300

    - comments (var_dump), reformat

--- selling_keywords/create_array_works_data.php
+++ selling_keywords/create_array_works_data.php
@@ -19,14 +19,6 @@ if (isset($_POST['keyword'])) {
 
 $image_type = $_POST['imageType'];
 
-//var_dump( $_POST['author'] );
-//var_dump( $_POST['keyword'] );
-//var_dump( $_POST['imageType'] );
-
-//var_dump( $author );
-//var_dump( $keyword );
-//var_dump( $image_type );
-
 require_once($_SERVER["DOCUMENT_ROOT"] . "/selling_keywords/arrays_cookies_agents.php");
 
 $useragent = RANDOM_SELECT_STRING($array_useragents);
@@ -34,27 +26,15 @@ $cookies = RANDOM_SELECT_STRING($array_cookies);
 
 if ($author == '') {
     $search_url = 'https://www.shutterstock.com/en/search/' . $keyword . '?image_type=' . $image_type;
-
-//    echo $search_url;
-
     $array_works_data = ARRAY_WORKS_DATA_JSON($search_url, $useragent, $cookies);
 } else {
     $search_url = 'https://www.shutterstock.com/g/' . $author . '?searchterm=' . $keyword . '&search_source=base_gallery&language=en&page=1&sort=popular&image_type=' . $image_type . '&measurement=px&safe=true';
-
-//    echo $search_url;exit;
-
     $array_works_data = ARRAY_WORKS_DATA_HTML($search_url, $useragent, $cookies);
 }
 
-//echo $search_url;
-//var_dump( $array_works_data );
-
 $url = CREATE_URL($array_works_data);
-//echo $url;
 $json_selling_keywords = USE_CURL($url, $useragent, $cookies);
-//echo $json_selling_keywords;
 $array_selling_keywords = json_decode($json_selling_keywords, true);
-//var_dump( $array_selling_keywords );
 
 foreach ($array_works_data as $element_1) {
     foreach ($array_selling_keywords as $element_2) {
@@ -93,13 +73,7 @@ function RANDOM_SELECT_STRING($_PARAM_array_strings)
 function ARRAY_WORKS_DATA_HTML($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies)
 {
     $data = USE_CURL($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies);
-
-//    echo $data;
-
     preg_match_all('/<img class="z_h_l z_h_c z_h_e".*?>/su', $data, $array_works_block);
-
-//    var_dump( $array_works_block );
-//    echo count( $array_works_block[0]);
 
     if (count($array_works_block[0]) == 0) {
         echo('-1');
@@ -113,7 +87,6 @@ function ARRAY_WORKS_DATA_HTML($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies)
         preg_match('/alt=".*?"/su', $element, $title);
         $title = preg_replace('/alt="/', '', $title);
         $title = preg_replace('/"/', '', $title);
-
         $img = $element;
 
         preg_match("/[0-9]*\.jpg/su", $element, $id);
@@ -138,7 +111,6 @@ function ARRAY_WORKS_DATA_HTML($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies)
 function ARRAY_WORKS_DATA_JSON($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies)
 {
     $data = USE_CURL($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies);
-
     preg_match_all('/<script data-react-helmet="true" type="application\/ld\+json">\[.*]<\/script>/su', $data, $array_works_block);
 
     if (count($array_works_block[0]) == 0) {
@@ -150,7 +122,6 @@ function ARRAY_WORKS_DATA_JSON($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies)
     $array_works_block[0][0] = preg_replace('/]<\/script>/', '', $array_works_block[0][0]);
     $array_works_block[0][0] = preg_replace('/},{/', '},,,,{', $array_works_block[0][0]);
     $array_works_block = explode(",,,,", $array_works_block[0][0]);
-
     $array_works_data = array();
 
     foreach ($array_works_block as $element) {
@@ -203,10 +174,6 @@ function USE_CURL($_PARAM_url, $_PARAM_useragent, $_PARAM_cookies)
     curl_setopt($SESSION, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($SESSION, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($SESSION);
-
-//    var_dump( curl_getinfo( $SESSION ) );
-//    var_dump( curl_getinfo( $SESSION, CURLINFO_EFFECTIVE_URL ) );
-//    var_dump( curl_getinfo( $SESSION, CURLINFO_REDIRECT_COUNT ) );
 
     curl_close($SESSION);
 
