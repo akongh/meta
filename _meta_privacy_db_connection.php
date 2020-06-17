90eac395303f52efd904e80fcd9fe380ea5166d7
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-17 23:37:34 +0300

    reorganize of DB connection

--- _meta_privacy_db_connection.php
+++ _meta_privacy_db_connection.php
@@ -3,10 +3,32 @@
 declare(strict_types=1);
 error_reporting(-1);
 
-if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/db_connection.php")) {
-    require($_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/db_connection.php");
-} elseif (file_exists($_SERVER["DOCUMENT_ROOT"] . "/_meta_privacy/db_connection.php")) {
-    require($_SERVER["DOCUMENT_ROOT"] . "/_meta_privacy/db_connection.php");
+$patch_to_db_connection = $_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/db_connection";
+
+if (file_exists($patch_to_db_connection)) {
+    $db_connection_data = file($patch_to_db_connection, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    if (4 === count($db_connection_data)) {
+        $server = $db_connection_data[0];
+        $user = $db_connection_data[1];
+        $password = $db_connection_data[2];
+        $name = $db_connection_data[3];
+    } else {
+        echo "Count of DB connection data is wrong.";
+        exit;
+    }
+
+    $mysqli = new mysqli($server, $user, $password, $name);
+
+    if ($mysqli->connect_errno) {
+        echo PHP_EOL . $mysqli->errno . PHP_EOL . $mysqli->error . PHP_EOL;
+        exit;
+    }
+
+    $mysqli->set_charset('utf8');
+    $mysqli->query("SET character_set_database=utf8");
+    $mysqli->query("SET NAMES utf8");
+    $mysqli->query("SET CHARACTER SET utf8");
 } else {
     echo "Database connection settings were not found.";
     exit;
