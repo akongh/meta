c418ef3b7dba704a484daf8b812d75866d8892ed
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-17 23:52:45 +0300

    reorganize of selling keywords sessions

--- selling_keywords/arrays_random_data.php
+++ selling_keywords/arrays_random_data.php
@@ -5,10 +5,14 @@ $array_useragents = [
     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36 OPR/45.0.2552.812'
 ];
 
-$array_cookies = [
-    'session=s%3Af7J3l5gDP1BR95IAl7jpU8dqu-3E_qB-.4d7tstVYNMoVuFadrGo1s0iNd9s7aO%2Bbf6TJX6ngSeI', // Elena
-    'session=s%3Alcb48Z6nJFqjxN-FvE0AaD5t9fLMNEsJ.obgs%2Bz902GABhwXOl%2B0GFEi%2BJ1WAir%2Bq3Zi%2B68lpHrM' // Andrei
-];
+$patch_to_selling_keywords_sessions = $_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/selling_keywords_sessions";
+
+if (file_exists($patch_to_selling_keywords_sessions)) {
+    $array_cookies = file($patch_to_selling_keywords_sessions, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+} else {
+    echo "Selling keywords sessions not found.";
+    exit;
+}
 
 $array_countries = [
 //    "AU",
