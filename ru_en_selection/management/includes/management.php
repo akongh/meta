54bba489bc130a173d0e48a3e3709eef629e7790
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-17 13:25:16 +0300

    link to tool from main page

--- ru_en_selection/management/includes/management.php
+++ ru_en_selection/management/includes/management.php
@@ -15,6 +15,7 @@
 <body class="bg">
 <div class="wrap">
     <?php
+    require($_SERVER["DOCUMENT_ROOT"] . "/link_to_index.php");
     require($_SERVER["DOCUMENT_ROOT"] . '/ru_en_selection/management/includes/management_title.php'); ?>
     <hr class="otbivka_48">
     <?php
