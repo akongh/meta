e02d30057a73d5b02c9dbac3e43416909fbc62ae
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-18 13:06:02 +0300

    Separation of message settings from code

--- get_message.php
+++ get_message.php
@@ -3,10 +3,13 @@
 declare(strict_types=1);
 error_reporting(-1);
 
+$patch_to_message_settings = $_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/message_settings";
+$message_settings = file($patch_to_message_settings, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
 if (isset($_POST["messageText"])) {
-    $from = "andreikorzhyts@gmail.com";
-    $to = "andreikorzhyts@gmail.com";
-    $subject = "META.afoteris.com";
+    $from = $message_settings[0];
+    $to = $message_settings[0];
+    $subject = $message_settings[1];
     $message = $_POST["messageText"];
     $headers = "From: <{$from}>\r\n" .
         "Reply-To: <{$to}>\r\n" .
