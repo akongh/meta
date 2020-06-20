696c3a8aa1594087b3e13b9b27f9cd8f2d4f1202
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-06-20 13:42:14 +0300

    impr separate data

--- selling_keywords/arrays_random_data.php
+++ selling_keywords/arrays_random_data.php
@@ -5,9 +5,9 @@ $array_useragents = [
     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36 OPR/45.0.2552.812'
 ];
 
-$patch_to_selling_keywords_sessions = $_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/selling_keywords_sessions";
-if (file_exists($patch_to_selling_keywords_sessions)) {
-    $array_cookies = file($patch_to_selling_keywords_sessions, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$patch_to_selling_keywords_cookies = $_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/selling_keywords_cookies";
+if (file_exists($patch_to_selling_keywords_cookies)) {
+    $array_cookies = file($patch_to_selling_keywords_cookies, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 } else {
     echo "Selling keywords sessions not found.";
     exit;
