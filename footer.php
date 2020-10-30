9f850d65f863bf40bf083bef0bd75e74cc71a28b
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-10-30 05:55:41 +0300

    updating of project date structure

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@ if (file_exists($patch_to_feedback_data)) {
 
 <div class="footer">
     META 0.4.1<br>
-    © Andrei Korzhyts, Elena Abrazhevich, 2014&mdash;2020
+    © Andrei Korzhyts, Elena Abrazhevich, 2014
     <?php
     if (isset($feedback_data)) {
         echo $feedback_data;
