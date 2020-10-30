589ef0fc46a3b75dc492b0ea947ea3526a6463cb
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-10-30 05:59:20 +0300

    author name was changed to "ru"

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@ if (file_exists($patch_to_feedback_data)) {
 
 <div class="footer">
     META 0.4.1<br>
-    © Andrei Korzhyts, Elena Abrazhevich, 2014
+    © А. П. Коржиц, Е. В. Абражевич, 2014
     <?php
     if (isset($feedback_data)) {
         echo $feedback_data;
