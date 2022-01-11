19b4401c2300b545f976e82bb5d57661b8e27f06
Andrei Korzhyts <>
2022-01-11 22:37:31 +0300

    reduce $amount to 100

--- selling_keywords/create_array_works_data.php
+++ selling_keywords/create_array_works_data.php
@@ -25,7 +25,7 @@ if ("all" == $_POST['imageType']) {
     $image_type = "&filter[image_type]={$_POST['imageType']}";
 }
 
-$amount = 200;
+$amount = 100;
 $useragent = RANDOM_SELECT_STRING($array_useragents);
 $cookies = RANDOM_SELECT_STRING($array_cookies);
 
