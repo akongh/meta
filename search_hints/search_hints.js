6e67046f7828d272e656389dcfaf498c30ecf9d9
Andrei Korzhyts <>
2022-05-16 19:41:05 +0300

    # and _ in hints from YouTube

--- search_hints/search_hints.js
+++ search_hints/search_hints.js
@@ -54,7 +54,7 @@ function newXMLHttpRequest(PARAM_url, PARAM_requestSet) {
                 document.querySelector("#error-hints").innerHTML = "Слишком длинное опорное слово.";
                 enableGetBasicKeywordsButton();
             } else if (request.responseText === "-2") {
-                document.querySelector("#error-hints").innerHTML = "Только латиница, цифры, пробел, дефис, апостроф и амперсанд.";
+                document.querySelector("#error-hints").innerHTML = "Только латиница, цифры, пробел, дефис, апостроф, амперсанд, октоторп и подчерк.";
                 enableGetBasicKeywordsButton();
             } else {
                 window.hintsObjectsArray = JSON.parse(request.responseText);
