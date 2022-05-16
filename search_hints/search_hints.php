9b10da2dd49ddcfa3739662eccb79c0e0da2dd15
Andrei Korzhyts <>
2022-05-16 18:13:40 +0300

    removed of commented markup

--- search_hints/search_hints.php
+++ search_hints/search_hints.php
@@ -58,31 +58,6 @@ require($_SERVER["DOCUMENT_ROOT"] . '/_meta_privacy_db_connection.php');
     <input type="submit"
            id="get-basic-keywords-button-youtube"
            value="От Ютуба">
-    <!--    <input type="submit"-->
-    <!--           id="get-basic-keywords-button-istockphoto"-->
-    <!--           value="От Айстокфото">-->
-    <!--    <input type="submit"-->
-    <!--           id="get-basic-keywords-button-getty"-->
-    <!--           value="От Геттиимаджес">-->
-    <!--    <input type="submit"-->
-    <!--           id="get-basic-keywords-button-fotolia"-->
-    <!--           value="От Фотолии">-->
-    <!--    <label><input type="radio"-->
-    <!--                  name="media_type_bigstockphoto"-->
-    <!--                  value="image"-->
-    <!--                  checked> Изображения</label>-->
-    <!--    <label><input type="radio"-->
-    <!--                  name="media_type_bigstockphoto"-->
-    <!--                  value="video"> Видео</label>-->
-    <!--    <input type="submit"-->
-    <!--           id="get-basic-keywords-button-bigstockphoto"-->
-    <!--           value="От Бигстокфото">-->
-    <!--    <input type="submit"-->
-    <!--           id="get-basic-keywords-button-depositphotos"-->
-    <!--           value="От Депозитфотос">-->
-    <!--    <input type="submit"-->
-    <!--           id="get-basic-keywords-button-123rf"-->
-    <!--           value="От 123РФ">-->
     <div id="hints-area" class="result">Список подсказок пуст.</div>
     <?php
     require($_SERVER["DOCUMENT_ROOT"] . '/footer.php'); ?>
