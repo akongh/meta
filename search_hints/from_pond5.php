06082c7444af1667bdfbefbb1f2bf32ad3cd53cb
Andrei Korzhyts <>
2022-07-12 21:43:57 +0300

    + Pond5

--- search_hints/from_pond5.php
+++ search_hints/from_pond5.php
@@ -54,7 +54,7 @@ echo($pond5_result);
  */
 function POND5_RESPONSE_FOR_ONE_BASIC_KEYWORD(string $_PARAM_basic_keyword, string $_PARAM_media_type)
 {
-    if ($_PARAM_basic_keyword != "") {
+    if ("" !== $_PARAM_basic_keyword) {
         $_PARAM_basic_keyword = preg_replace("/ /", "+", $_PARAM_basic_keyword);
     }
     if ("footage" === $_PARAM_media_type) {
