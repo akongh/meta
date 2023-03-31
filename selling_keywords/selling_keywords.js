bc983a8e773880017f1df6322e810b474d663909
Andrei Korzhyts <>
2023-03-31 17:05:54 +0300

    API compliance

--- selling_keywords/selling_keywords.js
+++ selling_keywords/selling_keywords.js
@@ -83,7 +83,7 @@ function getSellingKeywordsData() {
 function sendPapamsGetSellingKeywords(PARAM_url, PARAM_sellingKeywordsRequest, PARAM_useVariants, PARAM_sellingKeyword) {
     let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {//console.log(request.responseText);
+        if (request.readyState === 4 && request.status === 200) {console.log(request.responseText);
             if (request.responseText === '-1') {
                 if (typeof window.worksDataObjects === "undefined") {
                     document.querySelector("#selling-keywords-string").innerHTML = 'Шаттерсток ничего не выдал.';
