2e9d9337e46bcded591470fd2bced8c563257f0f
Andrei Korzhyts <>
2023-06-13 16:59:26 +0300

    search hints

--- set_compiler/set_compiler.js
+++ set_compiler/set_compiler.js
@@ -188,13 +188,13 @@ function createHTMLHintsList(PARAM_hintsObjectsArray) {
         }
         listResultArray[i] = "<div class='hint-box" + statusClass + "' title=''>" +
             "<span class='hint-hover'>" + PARAM_hintsObjectsArray[i].hint + "</span>" +
+            "</div>" +
             " → " +
             "<a href='https://www.youtube.com/results?search_query=" + PARAM_hintsObjectsArray[i].hint + "&sp=CAMSBAgFEAE%253D' target='_blank'>YT</a>" +
             " " +
             "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall</a>" +
             " " +
-            "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m</a>" +
-            "</div>";
+            "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m</a>";
     }
     document.querySelector("#hints-area").innerHTML = listResultArray.join("");
 
