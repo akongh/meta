350554958294d371119d3b25fe4da025cd4274b3
Andrei Korzhyts <>
2023-06-16 12:39:03 +0300

    common

--- search_hints/search_hints.js
+++ search_hints/search_hints.js
@@ -146,13 +146,13 @@ function createHTMLHintsListYoutube(PARAM_hintsObjectsArray) {
                 " → " +
                 "<a href='https://www.youtube.com/results?search_query=" + PARAM_hintsObjectsArray[i] + "&sp=CAMSBAgFEAE%253D' target='_blank'>YT</a>" +
                 " " +
-                "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GTall</a>" +
+                "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' class='a_highlight_1' target='_blank'>GTall</a>" +
                 " " +
                 "<a href='https://trends.google.com/trends/explore?cat=3&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GTall3</a>" +
                 " " +
                 "<a href='https://trends.google.com/trends/explore?cat=71&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GTall71</a>" +
                 " " +
-                "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GT12m</a>" +
+                "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' class='a_highlight_1' target='_blank'>GT12m</a>" +
                 " " +
                 "<a href='https://trends.google.com/trends/explore?cat=3&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GT12m3</a>" +
                 " " +
--- set_compiler/set_compiler.js
+++ set_compiler/set_compiler.js
@@ -193,13 +193,13 @@ function createHTMLHintsList(PARAM_hintsObjectsArray) {
             " → " +
             "<a href='https://www.youtube.com/results?search_query=" + PARAM_hintsObjectsArray[i].hint + "&sp=CAMSBAgFEAE%253D' target='_blank'>YT</a>" +
             " " +
-            "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall</a>" +
+            "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' class='a_highlight_1' target='_blank'>GTall</a>" +
             " " +
             "<a href='https://trends.google.com/trends/explore?cat=3&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall3</a>" +
             " " +
             "<a href='https://trends.google.com/trends/explore?cat=71&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall71</a>" +
             " " +
-            "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m</a>" +
+            "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' class='a_highlight_1' target='_blank'>GT12m</a>" +
             " " +
             "<a href='https://trends.google.com/trends/explore?cat=3&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m3</a>" +
             " " +
