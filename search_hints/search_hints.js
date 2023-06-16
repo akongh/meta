3e0c8910c659ed3f0464cebd69682749a7cbe0dc
Andrei Korzhyts <>
2023-06-16 11:37:46 +0300

    search hints

--- search_hints/search_hints.js
+++ search_hints/search_hints.js
@@ -148,7 +148,15 @@ function createHTMLHintsListYoutube(PARAM_hintsObjectsArray) {
                 " " +
                 "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GTall</a>" +
                 " " +
+                "<a href='https://trends.google.com/trends/explore?cat=3&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GTall3</a>" +
+                " " +
+                "<a href='https://trends.google.com/trends/explore?cat=71&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GTall71</a>" +
+                " " +
                 "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GT12m</a>" +
+                " " +
+                "<a href='https://trends.google.com/trends/explore?cat=3&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GT12m3</a>" +
+                " " +
+                "<a href='https://trends.google.com/trends/explore?cat=71&gprop=youtube&q=" + PARAM_hintsObjectsArray[i] + "' target='_blank'>GT12m71</a>" +
                 "</div>";
         }
     }
--- set_compiler/set_compiler.js
+++ set_compiler/set_compiler.js
@@ -193,9 +193,17 @@ function createHTMLHintsList(PARAM_hintsObjectsArray) {
             " → " +
             "<a href='https://www.youtube.com/results?search_query=" + PARAM_hintsObjectsArray[i].hint + "&sp=CAMSBAgFEAE%253D' target='_blank'>YT</a>" +
             " " +
-            "<a href='https://trends.google.com/trends/explore?cat=71&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall</a>" +
+            "<a href='https://trends.google.com/trends/explore?date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall</a>" +
             " " +
-            "<a href='https://trends.google.com/trends/explore?cat=71&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m</a>" +
+            "<a href='https://trends.google.com/trends/explore?cat=3&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall3</a>" +
+            " " +
+            "<a href='https://trends.google.com/trends/explore?cat=71&date=all_2008&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GTall71</a>" +
+            " " +
+            "<a href='https://trends.google.com/trends/explore?gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m</a>" +
+            " " +
+            "<a href='https://trends.google.com/trends/explore?cat=3&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m3</a>" +
+            " " +
+            "<a href='https://trends.google.com/trends/explore?cat=71&gprop=youtube&q=" + PARAM_hintsObjectsArray[i].hint + "' target='_blank'>GT12m71</a>" +
             "</p>";// todo: make a category select
     }
     document.querySelector("#hints-area").innerHTML = listResultArray.join("");
