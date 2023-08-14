4611d47881ac4bb04860768a3780429bdfdbcf2b
Andrei <>
2023-08-14 09:01:20 +0300

    META

--- search_hints/search_hints.php
+++ search_hints/search_hints.php
@@ -109,7 +109,7 @@ error_reporting(-1);
     <label><input type="radio"
                   name="country_code"
                   value="ru"> ru</label>
-    <p>Эти параметры пока только для Ютуба</p>
+<!--    <p>Эти параметры пока только для Ютуба</p>-->
     <!--/common parameters-->
     <hr class="otbivka_48">
     <!--youtube-->
@@ -118,31 +118,31 @@ error_reporting(-1);
            value="От Ютуба">
     <!--/youtube-->
     <!--shutterstock-->
-    <label><input type="radio"
-                  name="media_type_shutterstock"
-                  value="image"
-                  checked> Image</label>
-    <label><input type="radio"
-                  name="media_type_shutterstock"
-                  value="audio"> Audio</label>
-    <label><input type="radio"
-                  name="media_type_shutterstock"
-                  value="video"> Video</label>
-    <input type="submit"
-           id="get-basic-keywords-button-shutterstock"
-           value="От Шаттерстока">
+<!--    <label><input type="radio"-->
+<!--                  name="media_type_shutterstock"-->
+<!--                  value="image"-->
+<!--                  checked> Image</label>-->
+<!--    <label><input type="radio"-->
+<!--                  name="media_type_shutterstock"-->
+<!--                  value="audio"> Audio</label>-->
+<!--    <label><input type="radio"-->
+<!--                  name="media_type_shutterstock"-->
+<!--                  value="video"> Video</label>-->
+<!--    <input type="submit"-->
+<!--           id="get-basic-keywords-button-shutterstock"-->
+<!--           value="От Шаттерстока">-->
     <!--/shutterstock-->
     <!--pond5-->
-    <label><input type="radio"
-                  name="media_type_pond5"
-                  value="footage"
-                  checked> Footage</label>
-    <label><input type="radio"
-                  name="media_type_pond5"
-                  value="sfx"> SFX</label>
-    <input type="submit"
-           id="get-basic-keywords-button-pond5"
-           value="From Pond5">
+<!--    <label><input type="radio"-->
+<!--                  name="media_type_pond5"-->
+<!--                  value="footage"-->
+<!--                  checked> Footage</label>-->
+<!--    <label><input type="radio"-->
+<!--                  name="media_type_pond5"-->
+<!--                  value="sfx"> SFX</label>-->
+<!--    <input type="submit"-->
+<!--           id="get-basic-keywords-button-pond5"-->
+<!--           value="From Pond5">-->
     <!--/pond5-->
     <div id="hints-area" class="result">Список подсказок пуст.</div>
     <?php
