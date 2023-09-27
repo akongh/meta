869278c841aca8ff4c34d3922cc947f8ce535efe
Andrei Korzhyts <contactandreikorzhyts@gmail.com>
2023-09-27 18:07:23 +0300

    META

--- search_hints/get_and_check.php
+++ search_hints/get_and_check.php
@@ -6,7 +6,9 @@ error_reporting(-1);
 $php_input_to_array = explode("\n", mb_strtolower(file_get_contents("php://input")));
 $basic_keywords_string = $php_input_to_array[0];
 $related_parameter = $php_input_to_array[1];
-$related_parameter_2 = $php_input_to_array[2];
+if (isset($php_input_to_array[2])) {
+    $related_parameter_2 = $php_input_to_array[2];
+}
 $related_parameter_3 = $php_input_to_array[3];
 
 $data_width = 40;
