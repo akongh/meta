9a98ddf0394cdebf1c0c99c4367dae798491fbbf
Andrei Korzhyts <andreikcontact@gmail.com>
2026-07-24 21:08:04 +0300

    drink and production)

--- _meta_privacy_db_connection.php
+++ _meta_privacy_db_connection.php
@@ -6,19 +6,14 @@ error_reporting(-1);
 $patch_to_db_connection = $_SERVER["DOCUMENT_ROOT"] . "/../_meta_privacy/db_connection";
 
 if (file_exists($patch_to_db_connection)) {
-    $db_connection_data = file($patch_to_db_connection, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $db_connection_data = file($patch_to_db_connection, FILE_IGNORE_NEW_LINES);
 
-    if (4 === count($db_connection_data)) {
-        $server = $db_connection_data[0];
-        $user = $db_connection_data[1];
-        $password = $db_connection_data[2];
-        $name = $db_connection_data[3];
-    } else {
-        echo "Count of DB connection data is wrong.";
-        exit;
-    }
+    $server = $db_connection_data[0];
+    $user = $db_connection_data[1];
+    $password = $db_connection_data[2];
+    $db = $db_connection_data[3];
 
-    $mysqli = new mysqli($server, $user, $password, $name);
+    $mysqli = new mysqli($server, $user, $password, $db);
 
     if ($mysqli->connect_errno) {
         echo PHP_EOL . $mysqli->errno . PHP_EOL . $mysqli->error . PHP_EOL;
